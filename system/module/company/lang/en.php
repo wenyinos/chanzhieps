016cf06132b5831145570478bc02fddfd961eb35
wwccss <chunsheng@cnezsoft.com>
2013-09-24 16:21:28 +0800

    * change en file for company.

--- system/module/company/lang/en.php
+++ system/module/company/lang/en.php
@@ -1,42 +1,19 @@
 <?php
 /**
- * The site module english file of chanzhiEPS.
+ * The site module zh-cn file of chanzhiEPS.
  *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     site
+ * @package     company
  * @version     $Id$
  * @link        http://www.chanzhi.org
  */
-$lang->site->create   = "Create";
-$lang->site->edit     = "Edit";
-$lang->site->set      = "Set";
-$lang->site->delete   = "Delete";
+$lang->company->common  = "Company";
 
-$lang->site->id           = 'ID';
-$lang->site->name         = 'Name';
-$lang->site->code         = 'Code';
-$lang->site->domain       = 'Domain';
-$lang->site->logo         = 'Logo';
-$lang->site->keywords     = 'Keywords';
-$lang->site->slogan       = 'Slogan';
-$lang->site->mission      = 'Miassion';
-$lang->site->admins       = 'Admins';
-$lang->site->type         = 'Type';
-$lang->site->theme        = 'Theme';
-$lang->site->linkSites    = 'Link sites';
-$lang->site->indexModules = 'Index categories';
-$lang->site->menus        = 'Menu';
+$lang->company->name    = "Name";
+$lang->company->desc    = "Intro";
+$lang->company->content = "More";
 
-$lang->site->confirmDelete = "Are sure to delete this site?";
-$lang->site->successSaved  = "Success saved";
-$lang->site->addModule     = 'Add';
-$lang->site->deleteModule  = 'Delete';
-$lang->site->upModule      = 'Increase';
-$lang->site->downModule    = 'Decrease';
-
-$lang->site->types['portal'] = 'Portal';
-$lang->site->types['blog']   = 'Blog';
-
-$lang->site->themes['default'] = 'Default';
+$lang->company->setBasic   = "Basic";
+$lang->company->setContact = "Contact";
