9ca93c9e9457ceea2fa927874558eab91836f988
wwccss <chunsheng@cnezsoft.com>
2013-09-24 16:15:56 +0800

    * change the lang files for comment module.

--- system/module/comment/lang/zh-cn.php
+++ system/module/comment/lang/zh-cn.php
@@ -14,6 +14,7 @@ $lang->comment->id          = '编号';
 $lang->comment->author      = '称呼';
 $lang->comment->content     = '评论';
 $lang->comment->email       = '邮箱';
+$lang->comment->captcha       = '验证码';
 $lang->comment->list        = '评论列表';
 $lang->comment->post        = '发表评论';
 $lang->comment->viewArticle = '正文';
@@ -25,7 +26,6 @@ $lang->comment->reply         = '回复';
 $lang->comment->manage        = '评论管理';
 $lang->comment->delete        = '删除';
 $lang->comment->passPre       = '通过之前';
-$lang->comment->captcha       = '验证码';
 $lang->comment->deletePre     = '删除之前';
 $lang->comment->commentTo     = '发表于';
 $lang->comment->deletedObject = '已删除项目';
@@ -37,9 +37,3 @@ $lang->comment->confirmPassPre      = '您确定要通过之前的评论吗？';
 
 $lang->comment->statusList[0] = '未审核';
 $lang->comment->statusList[1] = '已审核';
-
-$lang->captcha = new stdclass();
-$lang->captcha->numbers = array('零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十');
-$lang->captcha->actions = array('*' => '乘', '-' => '减', '+' => '加');
-$lang->captcha->equal       = '=';
-$lang->captcha->notice      = '（填数字）';
