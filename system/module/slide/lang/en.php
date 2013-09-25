13b0dc984a0acd25fc989d5141551fe208a7267e
wwccss <chunsheng@cnezsoft.com>
2013-09-25 13:29:26 +0800

    * adjust lang files for slide module.

---
+++ system/module/slide/lang/en.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The slide category zh-cn file of chanzhiEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     slide
+ * @version     $Id$
+ * @link        http://www.chanzhi.org
+ */
+$lang->slide->common = 'Slides';
+
+$lang->slide->title    = 'Title';
+$lang->slide->image    = 'Image';
+$lang->slide->url      = 'URL';
+$lang->slide->summary  = 'Summary';
+$lang->slide->label    = 'Label';
+
+$lang->slide->sort     = 'Order';
+$lang->slide->saveSort = 'Save';
+$lang->slide->admin    = 'Admin';
+$lang->slide->create   = 'Add';
+$lang->slide->edit     = 'Edit';
+
+$lang->slide->successSave     = 'Successfully saved.';
+$lang->slide->successSort     = 'Order data saved.';
+$lang->slide->noImageSelected = 'No images selected';
+$lang->slide->suitableSize    = '1140px X 270px';
