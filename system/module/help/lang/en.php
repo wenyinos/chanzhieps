5024129834f0b5afa72b62bd18e987d9795872c5
wwccss <chunsheng@cnezsoft.com>
2013-09-25 11:02:39 +0800

    * adjust the en language for help module.

--- system/module/help/lang/en.php
+++ system/module/help/lang/en.php
@@ -1,51 +1,27 @@
 <?php
-$lang->rmb            = 'yuan';
-$lang->help->common   = 'Online help';
-$lang->help->books    = 'Tutorials';
-$lang->help->articles = 'Articles';
-$lang->help->back     = 'Back';
-$lang->help->donation = 'Donation';
+$lang->help->common       = 'Help';
+$lang->help->books        = 'Manuals';
+$lang->help->articles     = 'Articles';
+$lang->help->backtobooks  = 'Back to manuals';
+$lang->help->successSaved = 'Successfully saved.';
 
-$lang->help->title        = 'ZentaoPMS Online Help';
-$lang->help->id           = 'Id';
-$lang->help->field        = 'Field';
-$lang->help->explaination = 'Explaination';
-$lang->help->comment      = "Comment";
-$lang->help->labels       = "Name of attachment";
+$lang->help->id           = 'ID';
+$lang->help->namenotempty = "Name can't be empty";
+$lang->help->codenotempty = "Code can't be empty";
+$lang->help->codeunique   = 'Code with the same name already exists.';
+$lang->help->codealnum    = 'Code must be nubmers or letters';
 
-$lang->help->export->title   = 'Export help doc';
-$lang->help->export->lbl     = 'Chose content for exporting:';
-$lang->help->export->account = 'Authorized persons';
+$lang->book = new stdclass();
+$lang->book->create        = 'Create';
+$lang->book->edit          = 'Edit';
+$lang->book->id            = 'Id';
+$lang->book->name          = 'Name';
+$lang->book->summary       = 'Summary';
+$lang->book->code          = 'Code';
+$lang->book->directory     = 'Catalogue';
+$lang->book->articleList   = 'Articles';
+$lang->book->createArticle = 'Create an article';
 
-$lang->donation->anonymityList[0] = 'Not anonymous';
-$lang->donation->anonymityList[1] = 'Anonymous';
-
-$lang->donation->nologin   = "Dear user,you have not logged in. After <a href='%s'>login</a>, your account will be recorded as love account."; 
-$lang->donation->welcome   = "Dear %s, welcome to donate for zentao"; 
-$lang->donation->guest     = 'Warm-hearted people';
-$lang->donation->money     = 'Amount';
-$lang->donation->name      = 'Name';
-$lang->donation->email     = 'Email';
-$lang->donation->site      = 'Site';
-$lang->donation->anonymity = 'Anonymous';
-$lang->donation->donorList = 'Donor list';
-$lang->donation->donorMan  = 'Donor';
-$lang->donation->time      = 'Donation time';
-$lang->donation->confirm   = 'Donation confirm';
-$lang->donation->id        = 'Id';
-$lang->donation->subject   = 'Subject';
-$lang->donation->price     = 'Amount';
-$lang->donation->sorry     = 'Thank you for your kindness.';
-$lang->donation->thanks    = ' Thanks for donation! ';
-$lang->donation->payFail   = 'Sorry,paid failed. If you have problems, please contact us.';
-$lang->donation->view      = 'View donation';
-$lang->donation->useMoney  = 'Money for';
-$lang->donation->notice    = "The donation will be all for zentaopms development";
-
-$lang->donation->moneyList[10]   = 10;
-$lang->donation->moneyList[50]   = 50;
-$lang->donation->moneyList[100]  = 100;
-$lang->donation->moneyList[200]  = 200;
-$lang->donation->moneyList[300]  = 300;
-$lang->donation->moneyList[500]  = 500;
-$lang->donation->moneyList[1000] = 1000;
+/* Over write category translations of other module. */
+$lang->article->category = 'Catalogue';
+$lang->tree->manage      = 'Manage';
