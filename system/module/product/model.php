ab09360f5eec9c20d9740251d0d153087bb5717e
daitingting <daitingting@xirangit.com>
2013-09-27 10:56:23 +0800

    * add printfile function for product.

--- system/module/product/model.php
+++ system/module/product/model.php
@@ -242,4 +242,25 @@ class productModel extends model
            $this->dao->insert(TABLE_RELATION)->data($data)->exec();
        }
     }
+
+    /**
+     * Print files.
+     * 
+     * @param  object $files 
+     * @access public
+     * @return void
+     */
+    public function printFiles($files)
+    {
+        if(empty($files)) return false;
+
+        foreach($files as $file)
+        {
+            if(!$file->isImage)
+            {
+                $file->title = $file->title . ".$file->extension";
+                echo html::a(helper::createLink('file', 'download', "fileID=$file->id&mouse=left"), $file->title, '_blank') . '&nbsp;&nbsp;&nbsp'; 
+            }
+        }
+    }
 }
