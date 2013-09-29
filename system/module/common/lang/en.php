df62e79899223b1e75d065a166cf1d4477d7000a
daitingting <daitingting@xirangit.com>
2013-09-29 13:18:29 +0800

    * close the way of login by sina .

--- system/module/common/lang/en.php
+++ system/module/common/lang/en.php
@@ -135,7 +135,7 @@ $lang->site->menu->basic     = 'Basic|site|setbasic|';
 $lang->site->menu->logo      = 'Logo|site|setlogo|';
 $lang->site->menu->nav       = 'Navigation|nav|admin|';
 $lang->site->menu->slide     = array('link' => 'Slide|slide|admin|', 'alias' => 'create,edit');
-$lang->site->menu->sina      = 'Weibo OAuth|site|setappkey|';
+//$lang->site->menu->sina      = 'Weibo OAuth|site|setappkey|';
 
 /* Menu of company module. */
 $lang->company->menu = new stdclass();
