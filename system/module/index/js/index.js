f8aa1ee8102324e9ac4d32a7fc6adda7119a61a1
Catouse <catouse@gmail.com>
2013-10-10 16:56:08 +0800

    * change algorithm.

--- system/module/index/js/index.js
+++ system/module/index/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {          
     // auto ajust panel height.
     var fitPanelHeight = 0;
-    $('.row .panel').each(function(){if($(this).height()>fitPanelHeight) fitPanelHeight = $(this).height();}).height(fitPanelHeight);
+    $('.row .panel').each(function(){fitPanelHeight=Math.max($(this).height(),fitPanelHeight);}).height(fitPanelHeight);
 
     // add "index" class to the body element.
     $('body').addClass('index');
