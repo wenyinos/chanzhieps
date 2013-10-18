58dc5bf7204897745785ca10db4d4a4d5a4daca0
daitingting <daitingting@xirangit.com>
2013-10-18 16:09:54 +0800

    * rm unused code.

--- system/module/user/config.php
+++ system/module/user/config.php
@@ -8,13 +8,3 @@ $config->user->edit->requiredFields = 'realname,email';
 $config->user->default = new stdclass();
 $config->user->default->module = RUN_MODE == 'front' ? 'user'    : 'admin';
 $config->user->default->method = RUN_MODE == 'front' ? 'control' : 'index';
-
-$config->oauth       = new stdclass();
-$config->oauth->qq   = new stdclass();
-$config->oauth->sina = new stdclass();
-
-$config->oauth->qq->clientID     = '100541446';
-$config->oauth->qq->clientSecret = 'd92ba3b693ace4faf40e036bd0383080';
-
-$config->oauth->sina->clientID     = '708769934';
-$config->oauth->sina->clientSecret = 'bcacf0c76b1e5a713ccdef1db9cbba39';
