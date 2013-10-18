11429bf678a61a494a4727dcc461b186da5c4e57
guanxiying <guanxiying@xirangit.com>
2013-10-18 14:09:18 +0800

    * Add alias param when create rss link.

--- system/module/blog/view/footer.html.php
+++ system/module/blog/view/footer.html.php
@@ -16,7 +16,7 @@
       echo "&copy; {$config->company->name} {$config->site->copyright}-" . date('Y') . '&nbsp;&nbsp;';
       echo $config->site->icp;
       printf($lang->poweredBy, $config->version, $config->version);
-      echo html::a(helper::createLink('rss', 'index', '', 'xml') . '?type=blog', '<i class="icon icon-rss-sign icon-large"></i>', '_blank');
+      echo html::a(helper::createLink('rss', 'index', '', '', 'xml') . '?type=blog', '<i class="icon icon-rss-sign icon-large"></i>', '_blank');
       ?>
     </div>
   </footer>
