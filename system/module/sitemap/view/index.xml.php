705239ceae092d052d18028b4483174b0b2455d4
daitingting <daitingting@xirangit.com>
2013-10-24 08:54:08 +0800

    * adjust code.

--- system/module/sitemap/view/index.xml.php
+++ system/module/sitemap/view/index.xml.php
@@ -1,5 +1,35 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8" ?>';?>
 <urlset xmlns="http://www.google.com/schemas/sitemap/0.84">
+  <?php foreach($categories as $category): ?>
+  <url>
+    <?php 
+    if($category->type == 'product')
+    {
+        $url = $siteLink . helper::createLink('product', 'browse', "category={$category->id}", "category={$category->alias}");
+        echo "<loc>" . $url . "</loc>";
+    }
+    if($category->type == 'article')
+    {
+        $url = $siteLink . helper::createLink('article', 'browse', "category={$category->id}", "category={$category->alias}");
+        echo "<loc>" . $url . "</loc>";
+    }
+    if($category->type == 'blog')
+    {
+        $url = $siteLink . helper::createLink('blog', 'index', "category={$category->id}", "category={$category->alias}");
+        echo "<loc>" . $url . "</loc>";
+    }
+    if(strpos($category->type, 'book_') !== false)
+    {
+        $code = str_replace('book_', '', $category->type);
+        $url = $siteLink . helper::createLink('help', 'book', "type=$code&category={$category->id}", "category={$category->alias}");
+        echo "<loc>" . $url . "</loc>";
+    }
+    ?>
+    <lastmod><?php echo $category->postedDate;?></lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <?php endforeach;?>
   <?php
   foreach($products as $product):
   $categories = $product->categories;
@@ -47,36 +77,6 @@
     <lastmod><?php echo $product->editedDate == "0000-00-00 00:00:00" ? substr($prodcut->addedDate, 0, 10) : substr($product->editedDate, 0, 10);?></lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
-  </url>
-  <?php endforeach;?>
-  <?php foreach($categories as $category): ?>
-  <url>
-    <?php 
-    if($category->type == 'product')
-    {
-        $url = $siteLink . helper::createLink('product', 'browse', "category={$category->id}", "category={$category->alias}");
-        echo "<loc>" . $url . "</loc>";
-    }
-    if($category->type == 'article')
-    {
-        $url = $siteLink . helper::createLink('article', 'browse', "category={$category->id}", "category={$category->alias}");
-        echo "<loc>" . $url . "</loc>";
-    }
-    if($category->type == 'blog')
-    {
-        $url = $siteLink . helper::createLink('blog', 'index', "category={$category->id}", "category={$category->alias}");
-        echo "<loc>" . $url . "</loc>";
-    }
-    if(strpos($category->type, 'book_') !== false)
-    {
-        $code = str_replace('book_', '', $category->type);
-        $url = $siteLink . helper::createLink('help', 'book', "type=$code&category={$category->id}", "category={$category->alias}");
-        echo "<loc>" . $url . "</loc>";
-    }
-    ?>
-    <lastmod><?php echo $category->postedDate;?></lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.6</priority>
   </url>
   <?php endforeach;?>
 </urlset>
