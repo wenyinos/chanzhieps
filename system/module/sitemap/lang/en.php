8930764194ef758ff0f09c5c64dc9b640f1b1551
daitingting <daitingting@xirangit.com>
2013-10-25 13:23:07 +0800

    + add the translation for sitemap.

---
+++ system/module/sitemap/lang/en.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sitemap category zh-cn file of chanzhiEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@cnezsoft.com>
+ * @package     partner
+ * @version     $Id$
+ * @link        http://www.chanzhi.org
+ */
+$lang->sitemap->common          = 'Site map';
+$lang->sitemap->productCategory = 'Product category';
+$lang->sitemap->articleCategory = 'Article category';
+$lang->sitemap->blogCategory    = 'Blog category';
---
+++ system/module/sitemap/lang/zh-cn.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The sitemap category zh-cn file of chanzhiEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@cnezsoft.com>
+ * @package     partner
+ * @version     $Id$
+ * @link        http://www.chanzhi.org
+ */
+$lang->sitemap->common          = '站内地图';
+$lang->sitemap->productCategory = '产品分类';
+$lang->sitemap->articleCategory = '文章分类';
+$lang->sitemap->blogCategory    = '博客分类';
