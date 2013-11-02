571c404b15a4f9ee0b0250ec3d9a9aaf7247dd29
daitingting <daitingting@xirangit.com>
2013-11-02 19:34:23 +0800

    * rewrite index function.  * add sitemapXML function and sitemapHTML funtion.

--- system/module/sitemap/control.php
+++ system/module/sitemap/control.php
@@ -17,35 +17,59 @@ class sitemap extends control
      * @access public
      * @return void
      */
-    public function index()
+    public function index($onlyBody = 'no')
     {
-        if(strcmp(substr($_SERVER["REQUEST_URI"], -(strlen('sitemap.xml'))), 'sitemap.xml')==0) header("Content-type: text/xml");
+       if($this->app->getviewType() == 'html') die($this->sitemapHTML($onlyBody));
+       if($this->app->getviewType() == 'xml')  die($this->sitemapXML());
+    }
 
+    /**
+     * Output the sitemap.html.
+     * 
+     * @access public
+     * @return void
+     */
+    public function sitemapHTML($onlyBody)
+    {
+        $this->loadModel('tree');
+
+        $this->view->systemURL   = commonModel::getSysURL();
+        $this->view->articleTree = $this->tree->getTreeMenu('article', 0, array('treeModel', 'createBrowseLink'));
+        $this->view->productTree = $this->tree->getTreeMenu('product', 0, array('treeModel', 'createProductBrowseLink'));
+        $this->view->blogTree    = $this->tree->getTreeMenu('blog', 0, array('treeModel', 'createBlogBrowseLink'));
+        $this->view->boards      = $this->loadModel('forum')->getBoards();
+        $this->view->onlyBody    = $onlyBody;
+
+        $this->display();
+    }
+
+    /**
+     * Output sitemap.xml.
+     * 
+     * @access public
+     * @return void
+     */
+    public function sitemapXML()
+    {
         $this->loadModel('tree');
         $this->loadModel('article');
         $this->loadModel('product');
         $this->loadModel('forum');
         $this->loadModel('thread');
 
-        $this->app->loadClass('pager', $static = true);
-        $pager = new pager(0, $this->config->sitemap->items, 1);
+        $menus    = $this->dao->select('id, type, alias, editedDate, addedDate')->from(TABLE_ARTICLE)->where('type')->like("book_%")->fetchAll('id');
+        $articles = $this->article->getList('article', $this->tree->getFamily(0, 'article'), 'id_desc');
+        $blogs    = $this->article->getList('blog', $this->tree->getFamily(0, 'blog'), 'id_desc');
+        $products = $this->product->getList($this->tree->getFamily(0), 'id_desc');
+        $board    = $this->tree->getFamily(0);
+        $threads  = $this->dao->select('id, editedDate')->from(TABLE_THREAD)->beginIf($board)->where('board')->in($board)->orderBy('id desc')->fetchPairs();
 
-        $bookArticles = $this->dao->select('*')->from(TABLE_ARTICLE)->where('type')->like("book_%")->fi()->fetchAll('id');
-        $articles     = $this->article->getList('article', $this->tree->getFamily(0, 'article'), 'id_desc', $pager);
-        $blogs        = $this->article->getList('blog', $this->tree->getFamily(0, 'blog'), 'id_desc', $pager);
-        $products     = $this->product->getList($this->tree->getFamily(0), 'id_desc', $pager);
-        $threads      = $this->thread->getList($this->tree->getFamily(0), 'id_desc', $pager);
-
-        $this->view->siteLink     = commonModel::getSysURL();
-        $this->view->bookArticles = $bookArticles;
-        $this->view->articles     = $articles;
-        $this->view->blogs        = $blogs;
-        $this->view->products     = $products;
-        $this->view->threads      = $threads;
-        $this->view->articleTree  = $this->tree->getTreeMenu('article', 0, array('treeModel', 'createBrowseLink'));
-        $this->view->productTree  = $this->tree->getTreeMenu('product', 0, array('treeModel', 'createProductBrowseLink'));
-        $this->view->blogTree     = $this->tree->getTreeMenu('blog', 0, array('treeModel', 'createBlogBrowseLink'));
-        $this->view->boards       = $this->forum->getBoards();
+        $this->view->systemURL = commonModel::getSysURL();
+        $this->view->menus     = $menus;
+        $this->view->articles  = $articles;
+        $this->view->blogs     = $blogs;
+        $this->view->products  = $products;
+        $this->view->threads   = $threads;
 
         $this->display();
     }
