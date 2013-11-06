69e2f5d2b79b33f9beefac2269f0da505ad2245e
daitingting <daitingting@xirangit.com>
2013-11-06 14:49:00 +0800

    * adjust code.

--- system/module/rss/view/index.xml.php
+++ system/module/rss/view/index.xml.php
@@ -4,7 +4,7 @@
   <title><?php echo $title;?></title>
   <link><?php echo $siteLink;?></link>
   <description><?php echo $desc;?></description>
-  <copyright><?php echo '&copy;' . $config->company->name . $config->site->copyright . '-' . date('Y');?></copyright>
+  <copyright><?php echo $config->company->name . $config->site->copyright . '-' . date('Y');?></copyright>
   <lastBuildDate><?php echo $lastDate;?></lastBuildDate>
   
   <?php 
