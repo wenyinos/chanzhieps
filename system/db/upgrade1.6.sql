7be0a534d2a26c776a8ca75806b620c36f37edb1
daitingting <daitingting@xirangit.com>
2013-11-13 21:36:29 -0800

    * add kindeditor field in file table.

--- system/db/upgrade1.6.sql
+++ system/db/upgrade1.6.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `eps_user` ADD `fails` tinyint unsigned NOT NULL DEFAULT '0' AFTER `last`,
 ADD `locked` int(10) unsigned NOT NULL DEFAULT '0' AFTER `fails`;
-ALTER TABLE `eps_product` ADD `buyLink` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `alias`;
+ALTER TABLE `eps_product` ADD `buyLink` VARCHAR( 255 ) NOT NULL AFTER `alias`;
+ALTER TABLE `eps_file` ADD `kindeditor` ENUM( '0', '1' ) NOT NULL DEFAULT '0' AFTER `primary`;
