e1e94a603d4d32b2ead11c55ec174e91ad2fb194
daitingting <daitingting@xirangit.com>
2013-11-20 00:19:32 -0800

    + add product block.  * adjust style.

--- system/module/block/view/block/product.html.php
+++ system/module/block/view/block/product.html.php
@@ -1,20 +1,20 @@
 <?php
 /**
- * The article form view file of block module of chanzhiEPS.
+ * The product form view file of block module of chanzhiEPS.
  *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
- * @author      Xiying Guan <guanxiying@xirangit.com>
+ * @author      Tingting Dai <daitingting@xirangit.com>
  * @package     block
  * @version     $Id$
  * @link        http://www.chanzhi.org
 */
 ?>
 <?php include '../../common/view/chosen.html.php';?>
-<?php $categories = $this->loadModel('tree')->getOptionMenu('article');?>
+<?php $categories = $this->loadModel('tree')->getOptionMenu('product');?>
 <tr>
-  <th><?php echo $lang->block->articleType;?></th>
-  <td><?php echo html::select('params[articleTypeList]', $lang->block->articleTypeList, isset($block->content->articleTypeList) ? $block->content->articleTypeList : '', "class='select-4'");?></td>
+  <th><?php echo $lang->block->productType;?></th>
+  <td><?php echo html::select('params[productTypeList]', $lang->block->productTypeList, isset($block->content->productTypeList) ? $block->content->productTypeList : '', "class='select-4'");?></td>
 </tr>
 <tr>
   <th><?php echo $lang->block->limit;?></th>
