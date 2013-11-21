d55f8085bc8ffb42693bff3d567eeec6cb85bf57
guanxiying <guanxiying@xirangit.com>
2013-11-20 21:37:54 -0800

    * format codes.

--- system/module/block/js/setregion.js
+++ system/module/block/js/setregion.js
@@ -1,17 +1,17 @@
 $(document).ready(function()
 {
-    $(document).on('click', 'a.plus', function() { $(this).parents('tr').after($('#entry').html()); });
+    $(document).on('click', 'a.plus', function(){$(this).parents('tr').after($('#entry').html());});
 
-    /*Delete options*/
-    $(document).on('click', '.delete', function() { $(this).parents('tr').remove(); });
+    /* Delete options. */
+    $(document).on('click', '.delete', function(){$(this).parents('tr').remove();});
 
-   /* sort up. */
+   /* Sort up. */
     $(document).on('click', '.icon-arrow-up', function()
     {
         $(this).parents('tr').prev().before($(this).parents('tr')); 
     });
 
-    /* sort down. */
+    /* Sort down. */
     $(document).on('click', '.icon-arrow-down', function()
     { 
         var hasNext = $(this).parents('tr').next().find('.plus').size() > 0;
