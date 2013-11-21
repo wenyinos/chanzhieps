818599a227214bee416c84649ca2a9aafce2fff7
wyd621 <wyd621@163.com>
2013-11-20 22:02:12 -0800

    * add contact for front.

--- system/module/block/view/block/contact.html.php
+++ system/module/block/view/block/contact.html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The category form view file of block module of chanzhiEPS.
+ * The contact front view file of block module of chanzhiEPS.
  *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
@@ -10,69 +10,18 @@
  * @link        http://www.chanzhi.org
 */
 ?>
-<style>
-ul{padding-left:0px;}
-li{list-style:none; margin-bottom:5px;}
-.icon-plus, .icon-remove, .icon-arrow-up, .icon-arrow-down{cursor:pointer}
-</style>
-<?php include '../../common/view/chosen.html.php';?>
-<tr>
-  <th><?php echo $lang->block->content;?></th>
-  <td>
-    <ul>
-      <?php 
-      $block->content->item  = explode(',', $block->content->item);
-      $block->content->value = explode(',', $block->content->value);
-      foreach($block->content->item as $i => $item)
-      {
-          if(empty($item)) continue;
-
-          $entry  = "<li>";
-          $entry .= html::select("params[item][]", $this->lang->block->contact->itemList, $item, "class='select-2'");
-          $entry .= html::input("params[value][]", $block->content->value[$i], "class='text-5'");
-          $entry .= "<i class='icon-plus plus'></i> <i class='icon-remove remove'></i> <i class='icon-arrow-up'></i> <i class='icon-arrow-down'></i>";
-          $entry .= '</li>';
-          echo $entry;
-      }
-
-      $entry  = "<li>";
-      $entry .= html::select("params[item][]", $this->lang->block->contact->itemList, '', "class='select-2'");
-      $entry .= html::input("params[value][]", '', "class='text-5'");
-      $entry .= "<i class='icon-plus plus'></i> <i class='icon-remove remove'></i> <i class='icon-arrow-up'></i> <i class='icon-arrow-down'></i>";
-      $entry .= '</li>';
-      echo $entry;
-      ?>
-    </ul>  
-  </td>    
-</tr>
-</span>
-<script>
-$(function(){
-    $(document).on('click', '.plus', function()
-    {
-        $(this).parent().after('<li>' + $(this).parent().html() + '</li>');
-        $(this).parent().next().find('select').val('');
-        $(this).parent().next().find('input').val('');
-    });
-    /* sort up. */
-    $(document).on('click', '.icon-arrow-up', function()
-    {
-        $(this).parent().prev().before($(this).parent()); 
-    });
-
-    /* sort down. */
-    $(document).on('click', '.icon-arrow-down', function()
-    { 
-        var hasNext = $(this).parent().next().find('input').size() > 0;
-        if(hasNext) $(this).parent().next().after($(this).parent()); 
-    });
-
-    /* delete. */
-    $(document).on('click', '.remove', function()
-    {
-        var count = $(this).parent().parent().find('li').size();
-        if(count == 1) return;
-        $(this).parent().remove();
-    });
-});
-</script>
+<?php if(!isset($contact)) $contact = $this->loadModel('company')->getContact();?>
+<div id='contact' class="panel panel-default">
+  <div class="panel-heading">
+    <h4><?php echo $block->title;?></h4>
+  </div>
+  <div class="panel-body">
+    <?php foreach($contact as $item => $value):?>
+    <dl>
+      <dt><?php echo $lang->company->$item . $lang->colon;?></dt>
+      <dd><?php echo $value;?></dd>
+      <div class='c-both'></div>
+    </dl>
+    <?php endforeach;?>      
+  </div>
+</div>
