781bc11d3b8b0c0e01cce691d494f04cdd0a9980
wyd621 <wyd621@163.com>
2013-11-21 19:21:18 -0800

    * adjust for tree html.php.

--- system/module/block/view/block/articletree.html.php
+++ system/module/block/view/block/articletree.html.php
@@ -10,21 +10,25 @@
  * @link        http://www.chanzhi.org
 */
 ?>
-<?php $this->loadModel('tree');?>
-<?php $block->content = json_decode($block->content);?>
+<?php
+$this->loadModel('tree');
+$block->content = json_decode($block->content);
+$type           = str_replace('tree', '', strtolower($block->type));
+$browseLink     = $type == 'article' ? 'createBrowseLink' : 'create' . ucfirst($type) . 'BrowseLink';
+?>
 <?php if($block->content->showChildren):?>
-<?php $treeMenu = $this->tree->getTreeMenu('article', 0, array('treeModel', 'createBrowseLink'));?>
-<div class='box radius'> 
-  <h4 class='list-group-item list-group-title'><?php echo $block->title;?></h4>
+<?php $treeMenu = $this->tree->getTreeMenu($type, 0, array('treeModel', $browseLink));?>
+<div class='box radius panel panel-default'> 
+  <div class='panel-heading'><h4><?php echo $block->title;?></h4></div>
   <?php echo $treeMenu;?>
 </div>
 <?php else:?>
-<?php $topCategories = $this->tree->getChildren(0);?>
+<?php $topCategories = $this->tree->getChildren(0, $type);?>
 <div class='list-group'> 
   <strong class='list-group-item list-group-title'><?php echo $block->title;?></strong>
   <?php
   foreach($topCategories as $topCategory){
-      $browseLink = helper::createLink('article', 'browse', "categoryID={$topCategory->id}", "category={$topCategory->alias}");
+      $browseLink = helper::createLink($type, 'browse', "categoryID={$topCategory->id}", "category={$topCategory->alias}");
       if($category->name==$topCategory->name)
       {
           echo html::a($browseLink, "<i class='icon-folder-open-alt '></i>" . $topCategory->name, "id='category{$topCategory->id}' class='list-group-item active'");
