58f92148b646425428dbcaf91f0800881314ae1d
daitingting <daitingting@xirangit.com>
2013-11-21 17:31:14 -0800

    * change type length.

--- system/db/upgrade1.6.sql
+++ system/db/upgrade1.6.sql
@@ -2,3 +2,4 @@ ALTER TABLE `eps_user` ADD `fails` tinyint unsigned NOT NULL DEFAULT '0' AFTER `
 ADD `locked` int(10) unsigned NOT NULL DEFAULT '0' AFTER `fails`;
 ALTER TABLE `eps_product` ADD `mall` VARCHAR( 255 ) NOT NULL AFTER `alias`;
 ALTER TABLE `eps_file` ADD `editor` ENUM( '0', '1' ) NOT NULL DEFAULT '0' AFTER `primary`;
+ALTER TABLE `eps_block` CHANGE `type` `type` VARCHAR( 20 ) NOT NULL;
