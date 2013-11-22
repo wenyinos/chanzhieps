8daa1852493799b5347453167b1e7d72779fd038
wyd621 <wyd621@163.com>
2013-11-21 18:06:01 -0800

    * change for no set lang variable.

--- system/module/common/view/selectlang.html.php
+++ system/module/common/view/selectlang.html.php
@@ -1,5 +1,5 @@
 <?php $clientLang = $this->app->getClientLang();?>
-<a href='###' class='dropdown-toggle' data-toggle='dropdown' title='<?php echo $lang->setLang;?>'><i class='icon-globe icon-large'></i> &nbsp;<?php echo $config->langs[$clientLang]?><span class='caret'></span></a>
+<a href='###' class='dropdown-toggle' data-toggle='dropdown'><i class='icon-globe icon-large'></i> &nbsp;<?php echo $config->langs[$clientLang]?><span class='caret'></span></a>
 <ul class='dropdown-menu'>
   <?php
   $langs = $config->langs;
