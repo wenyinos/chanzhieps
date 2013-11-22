6e63b185a706cde12c328564c2ff1a1dcce03ea2
daitingting <daitingting@xirangit.com>
2013-11-21 19:07:55 -0800

    * adjust code.

--- system/module/block/view/block/producttree.html.php
+++ system/module/block/view/block/producttree.html.php
@@ -24,7 +24,7 @@
   <strong class='list-group-item list-group-title'><?php echo $block->title;?></strong>
   <?php
   foreach($topCategories as $topCategory){
-      $browseLink = helper::createLink('article', 'browse', "categoryID={$topCategory->id}", "category={$topCategory->alias}");
+      $browseLink = helper::createLink('product', 'browse', "categoryID={$topCategory->id}", "category={$topCategory->alias}");
       if($category->name==$topCategory->name)
       {
           echo html::a($browseLink, "<i class='icon-folder-open-alt '></i>" . $topCategory->name, "id='category{$topCategory->id}' class='list-group-item active'");
