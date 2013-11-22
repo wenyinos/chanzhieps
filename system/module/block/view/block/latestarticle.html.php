d0bdf9b68d59cd5dcfd0c5011816941dea33d61c
daitingting <daitingting@xirangit.com>
2013-11-21 21:45:57 -0800

    * delete unnecessary code.

--- system/module/block/view/block/latestarticle.html.php
+++ system/module/block/view/block/latestarticle.html.php
@@ -11,55 +11,57 @@
 */
 ?>
 <?php 
-$content        = json_decode($block->content);
-$latestArticles = $this->loadModel('article')->getLatest($content->category, $content->limit);
+$content  = json_decode($block->content);
+$type     = str_replace('article', '', strtolower($block->type));
+$method   = 'get' . ucfirst($type);
+$articles = $this->loadModel('article')->$method($content->category, $content->limit);
 ?>
 <?php if(isset($content->image)):?>
-  <div class='box radius'>
-    <h4 class='title'><?php echo $block->title;?></h4>
-    <ul class="media-list">
-      <?php 
-      foreach($latestArticles as $article):
-      $category = array_shift($article->categories);
-      $url = helper::createLink('article', 'view', "id=$article->id", "category={$category->alias}&name=$article->alias");
-      ?>
-      <li class="media">
-        <div class='media-body'>
-          <div class='image-box'>
-            <?php 
-            if(!empty($article->image))
-            {
-                $title = $article->image->primary->title ? $article->image->primary->title : $article->title;
-                echo html::a($url, html::image($article->image->primary->smallURL, "title='{$title}' class='thumbnail'" ));
-            }
-            else
-            {
-                echo html::a($url, html::image($themeRoot . 'default/images/main/noimage.gif', "class='thumbnail'" ));
-            }
-          ?>
-          </div>
-          <h3 class='media-heading'> <?php echo html::a($url, $article->title);?> <span class='label label-default'><?php echo substr($article->addedDate, 0, 10);?></span> </h3>
-          <p><?php echo $article->summary;?></p>
+<div class='box radius'>
+  <h4 class='title'><?php echo $block->title;?></h4>
+  <ul class="media-list">
+    <?php 
+    foreach($articles as $article):
+    $category = array_shift($article->categories);
+    $url = helper::createLink('article', 'view', "id=$article->id", "category={$category->alias}&name=$article->alias");
+    ?>
+    <li class="media">
+      <div class='media-body'>
+        <div class='image-box'>
+          <?php 
+          if(!empty($article->image))
+          {
+              $title = $article->image->primary->title ? $article->image->primary->title : $article->title;
+              echo html::a($url, html::image($article->image->primary->smallURL, "title='{$title}' class='thumbnail'" ));
+          }
+          else
+          {
+              echo html::a($url, html::image($themeRoot . 'default/images/main/noimage.gif', "class='thumbnail'" ));
+          }
+        ?>
         </div>
-        <hr>
-      </li>
-      <?php endforeach;?>
-    </ul>
-  </div>
+        <h3 class='media-heading'> <?php echo html::a($url, $article->title);?> <span class='label label-default'><?php echo substr($article->addedDate, 0, 10);?></span> </h3>
+        <p><?php echo $article->summary;?></p>
+      </div>
+      <hr>
+    </li>
+    <?php endforeach;?>
+  </ul>
+</div>
 <?php else:?>
-  <div class="panel panel-default">
-    <div class="panel-heading"><h3><?php echo $block->title;?></h3></div>
-    <div class="panel-body"><ul class='mg-zero pd-zero'>
-      <?php foreach($latestArticles as $article): ?>
-      <?php 
-      $category = array_shift($article->categories);
-      $url = helper::createLink('article', 'view', "id={$article->id}", "category={$category->alias}&name={$article->alias}");
-      ?>
-      <li class='latest-news'>
-        <i class='icon-chevron-right'></i>
-        <?php echo html::a($url, $article->title, "class='latest-news' title='{$article->title}'");?>
-      </li>
-      <?php endforeach;?>
-    </ul></div>
-  </div>
+<div class="panel panel-default">
+  <div class="panel-heading"><h3><?php echo $block->title;?></h3></div>
+  <div class="panel-body"><ul class='mg-zero pd-zero'>
+    <?php foreach($articles as $article): ?>
+    <?php 
+    $category = array_shift($article->categories);
+    $url = helper::createLink('article', 'view', "id={$article->id}", "category={$category->alias}&name={$article->alias}");
+    ?>
+    <li class='latest-news'>
+      <i class='icon-chevron-right'></i>
+      <?php echo html::a($url, $article->title, "class='latest-news' title='{$article->title}'");?>
+    </li>
+    <?php endforeach;?>
+  </ul></div>
+</div>
 <?php endif;?>
--- system/module/block/view/block/latestproduct.html.php
+++ system/module/block/view/block/latestproduct.html.php
@@ -11,66 +11,68 @@
 */
 ?>
 <?php 
-$content        = json_decode($block->content);
-$latestProducts = $this->loadModel('product')->getLatest($content->category, $content->limit);?>
+$content  = json_decode($block->content);
+$type     = str_replace('product', '', strtolower($block->type));
+$method   = 'get' . $type;
+$products = $this->loadModel('product')->$method($content->category, $content->limit);?>
 <?php if(isset($content->image)):?>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class='title'><?php echo $block->title?></h4>
-    </div>
-    <div class="panel-body">
-      <ul class="media-list">
-        <?php foreach($latestProducts as $product):?>
-        <li class='media'>
-          <?php 
-          $productCategory = array_shift($product->categories);
-          $url = helper::createLink('product', 'view', "id=$product->id", "category={$productCategory->alias}&name=$product->alias");
-          $title = $product->image->primary->title ? $product->image->primary->title : $product->name;
-          if(empty($product->image)) 
-          {
-              echo html::a($url, html::image($themeRoot . 'default/images/main/noimage.gif', "title='{$title}' class='adaptive'"), "class='media-image'");
-          }
-          else
-          {
-              echo html::a($url, html::image($product->image->primary->middleURL, "title='{$title}' class='adaptive'"), "class='media-image'");
-          }
-          ?>
-          <div class='media-body'>
-            <h5 class='media-heading'><?php echo html::a($url, $product->name);?></h5>
-            <?php if($product->promotion != 0 && $product->price != 0):?>
-            <p>
-              <del><?php echo $lang->RMB . $product->price;?></del>
-              <em><?php echo $lang->RMB . $product->promotion;?></em>
-            </p>
-            <?php elseif($product->promotion == 0 && $product->price != 0):?>
-            <p><em><?php echo $lang->product->price . $lang->RMB . $product->price;?></em></p>
-            <?php elseif($product->promotion != 0 && $product->price == 0):?>
-            <p><em><?php echo $lang->product->promotion . $lang->RMB . $product->promotion;?></em></p>
-            <?php endif;?>
-          </div>
-        </li>
-        <?php endforeach;?>
-      </ul>      
-    </div>
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h4 class='title'><?php echo $block->title?></h4>
   </div>
+  <div class="panel-body">
+    <ul class="media-list">
+      <?php foreach($products as $product):?>
+      <li class='media'>
+        <?php 
+        $productCategory = array_shift($product->categories);
+        $url = helper::createLink('product', 'view', "id=$product->id", "category={$productCategory->alias}&name=$product->alias");
+        $title = $product->image->primary->title ? $product->image->primary->title : $product->name;
+        if(empty($product->image)) 
+        {
+            echo html::a($url, html::image($themeRoot . 'default/images/main/noimage.gif', "title='{$title}' class='adaptive'"), "class='media-image'");
+        }
+        else
+        {
+            echo html::a($url, html::image($product->image->primary->middleURL, "title='{$title}' class='adaptive'"), "class='media-image'");
+        }
+        ?>
+        <div class='media-body'>
+          <h5 class='media-heading'><?php echo html::a($url, $product->name);?></h5>
+          <?php if($product->promotion != 0 && $product->price != 0):?>
+          <p>
+            <del><?php echo $lang->RMB . $product->price;?></del>
+            <em><?php echo $lang->RMB . $product->promotion;?></em>
+          </p>
+          <?php elseif($product->promotion == 0 && $product->price != 0):?>
+          <p><em><?php echo $lang->product->price . $lang->RMB . $product->price;?></em></p>
+          <?php elseif($product->promotion != 0 && $product->price == 0):?>
+          <p><em><?php echo $lang->product->promotion . $lang->RMB . $product->promotion;?></em></p>
+          <?php endif;?>
+        </div>
+      </li>
+      <?php endforeach;?>
+    </ul>      
+  </div>
+</div>
 <?php else:?>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class='title'><?php echo $block->title?></h4>
-    </div>
-    <div class="panel-body">
-      <ul>
-        <?php foreach($latestProducts as $product):?>
-        <li>
-          <?php 
-          $productCategory = array_shift($product->categories);
-          $url = helper::createLink('product', 'view', "id=$product->id", "category={$productCategory->alias}&name=$product->alias");
-          $title = $product->image->primary->title ? $product->image->primary->title : $product->name;
-          ?>
-          <i class='icon-chevron-right'></i><?php echo html::a($url, $product->name);?>
-        </li>
-        <?php endforeach;?>
-      </ul>      
-    </div>
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h4 class='title'><?php echo $block->title?></h4>
   </div>
+  <div class="panel-body">
+    <ul>
+      <?php foreach($products as $product):?>
+      <li>
+        <?php 
+        $productCategory = array_shift($product->categories);
+        $url = helper::createLink('product', 'view', "id=$product->id", "category={$productCategory->alias}&name=$product->alias");
+        $title = $product->image->primary->title ? $product->image->primary->title : $product->name;
+        ?>
+        <i class='icon-chevron-right'></i><?php echo html::a($url, $product->name);?>
+      </li>
+      <?php endforeach;?>
+    </ul>      
+  </div>
+</div>
 <?php endif;?>
