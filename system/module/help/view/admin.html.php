5eecfdb252a6c865b1270332a559e41ba2285d69
daitingting <daitingting@xirangit.com>
2013-11-28 01:16:56 -0800

    * show the first book in admin page.

--- system/module/help/view/admin.html.php
+++ system/module/help/view/admin.html.php
@@ -11,35 +11,12 @@
  */
 ?>
 <?php include '../../common/view/header.admin.html.php';?>
-<div class="col-md-12">
-  <table class='table table-hover table-bordered table-striped'>
-    <caption>
-      <div class='f-left'><?php echo $lang->help->books;?></div>
-      <div class='f-right'><?php echo html::a($this->inlink('createBook'), $lang->book->create);?></div>
-    </caption>
-    <thead>
-      <tr class='a-center'>
-        <th class='w-p20'><?php echo $lang->book->name;?></th>
-        <th><?php echo $lang->book->summary;?></th>
-        <th class='w-200px'><?php echo $lang->actions;?></th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach($books as $key => $book):?>
-      <tr class='a-left v-middle'>
-        <td><?php echo $book->name;?></td>
-        <td><?php echo $book->summary;?></td>
-        <td class='a-center'>
-          <?php
-          echo html::a($this->createLink('article', 'admin', "type=book_$book->key"), $lang->book->articleList);
-          echo html::a($this->createLink('tree', 'browse', "type=book_$book->key"), $lang->book->directory);
-          echo html::a($this->createLink('help', 'deletebook', "id=$book->id"), $lang->delete, "class='deleter'");
-          echo html::a($this->createLink('help', 'editbook', "id=$book->id"), $lang->edit);
-          ?>
-        </td>
-      </tr>
-      <?php endforeach;?>
-    </tbody>
-  </table>
+<?php 
+$path = array_keys($book->pathNames);
+js::set('path', json_encode($path));
+?>
+<div class='box radius'>  
+  <h4 class='title'><?php echo $book->title;?></h4>
+  <dl><?php echo $this->help->getBookCatalogue($book->id);?></dl>
 </div>
 <?php include '../../common/view/footer.admin.html.php';?>
