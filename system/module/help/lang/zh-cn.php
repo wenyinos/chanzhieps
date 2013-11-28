1d628db39a2e1e73acb566f276f3ab528684b020
daitingting <daitingting@xirangit.com>
2013-11-28 01:41:30 -0800

    - rm unuse code.  * adjust translation.

--- system/module/help/lang/zh-cn.php
+++ system/module/help/lang/zh-cn.php
@@ -5,22 +5,20 @@ $lang->help->articles     = '文档导航';
 $lang->help->backtobooks  = '返回手册列表';
 
 $lang->help->id           = '编号';
-$lang->help->namenotempty = '名称不能为空';
-$lang->help->codenotempty = '编码不能为空';
-$lang->help->codeunique   = '此编码已经存在';
-$lang->help->codealnum  = '编码必须为英文字母或者数字';
 
 $lang->book = new stdclass();
 $lang->book->create        = '添加手册';
 $lang->book->edit          = '编辑手册';
 $lang->book->id            = '编号';
-$lang->book->name          = '手册名称';
-$lang->book->summary       = '手册简介';
-$lang->book->code          = '编码';
-$lang->book->directory     = '目录管理';
+$lang->book->parent        = '类目';
+$lang->book->author        = '作者';
+$lang->book->editor        = '编辑者';
+$lang->book->title         = '标题';
+$lang->book->keywords      = '关键词';
+$lang->book->summary       = '简介';
+$lang->book->content       = '内容';
+$lang->book->alias         = '别名';
+$lang->book->chapter       = '目录管理';
 $lang->book->articleList   = '文章管理';
+$lang->book->createChapter = '添加目录';
 $lang->book->createArticle = '发布文章';
-
-/* Over write category translations of other module. */
-$lang->article->category = '目录';
-$lang->tree->manage      = '维护目录';
