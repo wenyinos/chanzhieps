9db86a3a6d991f692acffb2afa865fc10731aa51
daitingting <daitingting@xirangit.com>
2013-11-28 01:37:37 -0800

    * adjust highlight.

--- system/module/help/js/common.js
+++ system/module/help/js/common.js
@@ -1,5 +1,14 @@
 $(document).ready(function()
 {
-    $('.nav-system-help').addClass('active');
+    $('.leftmenu li.active').removeClass('active');
+    
+    /* Set current active topNav. */
+    if(v.path && v.path.length)
+    {
+        $.each(eval(v.path), function(index, bookID) 
+        { 
+            $('.leftmenu a[href*=' + bookID + ']').parent().addClass('active');
+        })
+    }
 });
 
