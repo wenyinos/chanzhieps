bb623e5974fda0c0aa0cf73ab6dc7052d676a900
wyd621 <wyd621@163.com>
2013-11-28 18:31:08 -0800

    * add status field for produc.

--- system/db/upgrade1.7.sql
+++ system/db/upgrade1.7.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `eps_thread` CHANGE `readonly` `readonly` tinyint(1) NOT NULL DEFAULT '0' AFTER `editedDate`;
+ALTER TABLE `eps_product` ADD `status` varchar(20) COLLATE 'utf8_general_ci' NOT NULL DEFAULT 'normal' AFTER `editedDate`;
