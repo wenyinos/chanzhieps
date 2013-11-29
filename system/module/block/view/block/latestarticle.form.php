3397babdda253c33e707538d0f347f87afc381f6
Catouse <catouse@gmail.com>
2013-11-29 15:56:14 +0800

    * added class name '.form-control'.

--- system/module/block/view/block/latestarticle.form.php
+++ system/module/block/view/block/latestarticle.form.php
@@ -14,11 +14,11 @@
 <?php $categories = $this->loadModel('tree')->getOptionMenu('article');?>
 <tr>
   <th><?php echo $lang->block->categories;?></th>
-  <td><?php echo html::select('params[category][]', $categories, isset($block) ? $block->content->category : '', "class='text-4 chosen' multiple='multiple'");?></td>
+  <td><?php echo html::select('params[category][]', $categories, isset($block) ? $block->content->category : '', "class='text-4 form-control chosen' multiple='multiple'");?></td>
 </tr>
 <tr>
   <th><?php echo $lang->block->limit;?></th>
-  <td><?php echo html::input('params[limit]', isset($block->content->limit) ? $block->content->limit : '', "class='text-4'");?></td>
+  <td><?php echo html::input('params[limit]', isset($block->content->limit) ? $block->content->limit : '', "class='text-4 form-control'");?></td>
 </tr>
 <tr>
   <th><?php echo $lang->block->showImage;?></th>
