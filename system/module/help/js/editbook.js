795ced0bea8a3423871ec9463d5505c20d7ce8af
daitingting <daitingting@xirangit.com>
2013-12-01 19:26:51 -0800

    * mv help to book.

--- system/module/help/js/editbook.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function()
-{
-    $('.leftmenu li.active').removeClass('active');
-    $('.leftmenu a[href*=_' + v.type + ']').parent().addClass('active');
-});
--- system/module/help/js/read.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function()
-{
-    $('#commentBox').load( createLink('comment', 'show', 'objectType=article&objectID=' + v.articleID) );  
-});
