93586edd3491a1f55f73129a2ec58b4b876f827b
wyd621 <wyd621@163.com>
2013-12-01 18:45:28 -0800

    * finish task #467.

--- www/js/kindeditor/plugins/image/image.js
+++ www/js/kindeditor/plugins/image/image.js
@@ -47,7 +47,7 @@ KindEditor.plugin('image', function(K) {
 			'<label for="remoteUrl" style="width:60px;">' + lang.remoteUrl + '</label>',
 			'<input type="text" id="remoteUrl" class="ke-input-text" name="url" value="" style="width:200px;" /> &nbsp;',
 			'<span class="ke-button-common ke-button-outer">',
-			'<input type="hidden" class="ke-button-common ke-button" name="viewServer" value="' + lang.viewServer + '" />',
+			'<input type="button" class="ke-button-common ke-button" name="viewServer" value="' + lang.viewServer + '" />',
 			'</span>',
 			'</div>',
 			//size
@@ -296,7 +296,17 @@ KindEditor.plugin('image', function(K) {
 				showLocal : allowImageUpload,
 				tabIndex: img ? 0 : imageTabIndex,
 				clickFn : function(url, title, width, height, border, align) {
-					self.exec('insertimage', url, title, width, height, border, align);
+					if (img) {
+						img.attr('src', url);
+						img.attr('data-ke-src', url);
+						img.attr('width', width);
+						img.attr('height', height);
+						img.attr('title', title);
+						img.attr('align', align);
+						img.attr('alt', title);
+					} else {
+						self.exec('insertimage', url, title, width, height, border, align);
+					}
 					// Bugfix: [Firefox] 上传图片后，总是出现正在加载的样式，需要延迟执行hideDialog
 					setTimeout(function() {
 						self.hideDialog().focus();
