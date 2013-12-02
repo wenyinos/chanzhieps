2e0f4a7b7079a99b941d77e0ad799621ed02fbf3
daitingting <daitingting@xirangit.com>
2013-12-02 01:52:28 -0800

    * mv book to browse.

--- system/module/book/view/browse.html.php
+++ system/module/book/view/browse.html.php
@@ -3,12 +3,12 @@
 <div class='row'>
   <div class='col-md-3' id='leftmenu'>
     <div class="list-group">
-      <strong class="list-group-item list-group-title"><?php echo $lang->help->books;?></strong>
+      <strong class="list-group-item list-group-title"><?php echo $lang->book->list;?></strong>
       <?php
       foreach($books as $bookValue)
       {
           $class = 'list-group-item' . (($bookValue->title == $book->title) ? ' active' : '');
-          echo html::a(inlink('book', "bookID=$bookValue->id"), '<i class="icon-book icon-large"></i>' . $bookValue->title . '<i class="icon-chevron-right"></i>', "class='$class'");
+          echo html::a(inlink('browse', "bookID=$bookValue->id"), '<i class="icon-book icon-large"></i>' . $bookValue->title . '<i class="icon-chevron-right"></i>', "class='$class'");
       }
       ?>
     </div>
@@ -18,7 +18,7 @@
     <div class='box radius'>  
       <h4 class='title'><?php echo $book->title;?></h4>
       <dl>
-      <?php echo $this->help->getBookCatalogue($book->id) ?>
+      <?php echo $this->book->getBookCatalogue($book->id) ?>
       </dl>
     </div>
   </div>
