1e52c0856a367fd6ff7423a6afcd9313f15c8704
guanxiying <guanxiying@xirangit.com>
2013-12-01 21:24:00 -0800

    * Add <pre> to allowed tags.

--- system/config/config.php
+++ system/config/config.php
@@ -34,7 +34,7 @@ $config->sessionVar  = RUN_MODE . 'sid';  // The session var name.
 
 /* Set the allowed tags.  */
 $config->allowedTags = new stdclass();
-$config->allowedTags->front = '<p><span><h1><h2><h3><h4><h5><em><u><strong><br><ol><ul><li><img><a><b><font><hr>';    // For front mode.
+$config->allowedTags->front = '<p><span><h1><h2><h3><h4><h5><em><u><strong><br><ol><ul><li><img><a><b><font><hr><pre>';    // For front mode.
 $config->allowedTags->admin = $config->allowedTags->front . '<div><table><td><th><tr><tbody>';                             // For admin users.
 
 /* Views and themes. */
