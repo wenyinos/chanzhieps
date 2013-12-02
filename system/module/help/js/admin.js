9657726ef93df54c724be952e1370b1a622a3439
daitingting <daitingting@xirangit.com>
2013-12-01 18:48:38 -0800

    * mv highlight js to admin.js

--- system/module/help/js/admin.js
+++ system/module/help/js/admin.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
     $('.leftmenu li.active').removeClass('active');
     
-    /* Set current active menu. */
+    /* Set current active moduleMenu. */
     if(v.path && v.path.length)
     {
         $.each(eval(v.path), function(index, bookID) 
