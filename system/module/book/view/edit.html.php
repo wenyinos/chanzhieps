170aa493a84486c2b5dfbc445dec5c8af5582fd1
daitingting <daitingting@xirangit.com>
2013-12-02 01:53:33 -0800

    * adjust help to book.

--- system/module/book/view/edit.html.php
+++ system/module/book/view/edit.html.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * The edit book view file of help of chanzhiEPS.
+ * The edit book view file of book of chanzhiEPS.
  *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL
  * @author      Tingting Dai<daitingting@xirangit.com>
- * @package     help
+ * @package     book
  * @version     $Id$
  * @link        http://www.chanzhi.org
  */
