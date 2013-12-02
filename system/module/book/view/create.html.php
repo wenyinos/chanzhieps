a804d305d9be12f2a58ddf702c209413389d587d
daitingting <daitingting@xirangit.com>
2013-12-02 01:51:58 -0800

    * adjust sort.

--- system/module/book/view/create.html.php
+++ system/module/book/view/create.html.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * The create book view file of help of chanzhiEPS.
+ * The create book view file of book of chanzhiEPS.
  *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL
  * @author      Tingting Dai<daitingting@xirangit.com>
- * @package     help
+ * @package     book
  * @version     $Id$
  * @link        http://www.chanzhi.org
  */
@@ -55,33 +55,33 @@
       </thead>
       <tbody id='entry'>
         <?php
-        $maxOrder = 0;
         foreach($catalogues as $catalogue):
         ?>
         <tr class='v-middle'>
         <?php
-          if($book->order > $maxOrder) $maxOrder = $catalogue->order;
           echo '<td>' . html::select("type[$catalogue->id]", $lang->book->typeList, $catalogue->type, "class='select-2'") . '</td>';
           echo '<td>' . html::input("title[$catalogue->id]", $catalogue->title, "class='text-1'") . '</td>';
           echo "<td><i class='icon-arrow-up'></i><i class='icon-arrow-down'></i></td>";
+          echo '<td>' . html::hidden("order[$catalogue->id]", $catalogue->order, "class='order'") . '</td>';
           echo '<td>' . html::hidden("mode[$catalogue->id]", 'update') . '</td>';
         ?>
         </tr>
         <?php endforeach;?>
         <?php
-        for($i = 0; $i < HELP::NEW_CATALOGUE_COUNT ; $i ++)
+        for($i = 0; $i < BOOK::NEW_CATALOGUE_COUNT ; $i ++)
         {
            echo '<tr>';
            echo '<td>' . html::select("type[]", $lang->book->typeList, '', "class='select-2'") . '</td>';
            echo '<td>' . html::input("title[]", '', "class='text-1'") . '</td>';
            echo "<td><i class='icon-arrow-up'></i> &nbsp;<i class='icon-arrow-down'></i></td>";
+           echo '<td>' . html::hidden("order[]", '', "class='order'") . '</td>';
            echo '<td>' . html::hidden("mode[]", 'new') . '</td>';
            echo '</tr>';
         }
         ?>
       </tbody>
       <tfoot>
-        <tr><td colspan='2' class='a-center'><?php echo html::submitButton() . html::hidden('parent', $parent) . html::hidden('maxOrder', $maxOrder);?></td></tr>
+        <tr><td colspan='2' class='a-center'><?php echo html::submitButton() . html::hidden('parent', $parent);?></td></tr>
       </tfoot>
     </table>
   </form>
