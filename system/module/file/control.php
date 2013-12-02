39d35c57ede0a7054015e7a23bf4c41c36c3dd29
wyd621 <wyd621@163.com>
2013-12-01 18:01:47 -0800

    * revert file for delete method.

--- system/module/file/control.php
+++ system/module/file/control.php
@@ -284,7 +284,7 @@ class file extends control
      */
     public function delete($fileID)
     {
-        $this->file->delete($fileID);
+        $this->dao->delete()->from(TABLE_FILE)->where('id')->eq($fileID)->exec();
         if(!dao::isError()) $this->send(array('result' => 'success')); 
         $this->send(array('result' => 'fail', 'message' => dao::getError())); 
     }
