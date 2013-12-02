e04d113c47b7d4fb6c11e6443576bbfab0fcf77e
wyd621 <wyd621@163.com>
2013-12-01 19:03:33 -0800

    * adjust import js and css.

--- system/module/common/view/treeview.html.php
+++ system/module/common/view/treeview.html.php
@@ -1,3 +1,3 @@
-<link rel='stylesheet' href='<?php echo $themeRoot;?>default/treeview.css' type='text/css' />
-<script src='<?php echo $jsRoot;?>jquery/treeview/min.js' type='text/javascript'></script>
+<?php css::import($themeRoot . 'default/treeview.css');?>
+<?php js::import($jsRoot . 'jquery/treeview/min.js');?>
 <script language='javascript'>$(function() {$(".tree").treeview({collapsed: false, unique: false}) })</script>
