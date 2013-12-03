1dcb2182f3c0131b4d002bfaf9ab02412089bc25
daitingting <daitingting@xirangit.com>
2013-12-03 17:35:01 +0800

    * adjust read function.  + add up and down funcion for sort.

--- system/module/book/control.php
+++ system/module/book/control.php
@@ -16,8 +16,8 @@ class book extends control
     public function index()
     {
         $book = $this->book->getFirstBook();
-        if($book) $this->locate(inlink('browse', "type=$book->alias&book=$book->id"));
-        $this->locate($this->createLink('index'));
+        if($book) $this->locate(inlink('browse', "bookID=$book->id", "book=$book->alias"));
+        $this->locate(inlink('browse'));
     }
 
     /**
@@ -50,9 +50,10 @@ class book extends control
             $book = $this->book->getByID(array_shift($books)->id);
         } 
 
-        $this->view->title = $this->lang->book->common;
-        $this->view->books = $books;
-        $this->view->book  = $book;
+        $this->view->title     = $this->lang->book->common;
+        $this->view->books     = $books;
+        $this->view->book      = $book;
+        $this->view->catalogue = $this->book->getAdminCatalogue($book->id);
         $this->display();
     }
 
@@ -65,17 +66,17 @@ class book extends control
      */
     public function browse($bookID)
     {
-        $book          = $this->book->getByID($bookID);
-        $bookCatalogue = $this->book->getChildren($bookID);
+        $book     = $this->book->getByID($bookID);
+        $parent   = $this->book->getByID($book->parent);
 
         $this->view->title = $book->title;
-        if($bookCatalogue)
+        if($parent)
         {
-            $this->view->keywords = trim($bookCatalogue->keyword . ' ' . $this->config->site->keywords);
+            $this->view->keywords = trim($parent->keywords . ' ' . $this->config->site->keywords);
         }
-        $this->view->books      = $this->book->getBookList();
-        $this->view->book       = $book;
-        $this->view->category   = $bookCatalogue;
+        $this->view->books     = $this->book->getBookList();
+        $this->view->book      = $book;
+        $this->view->catalogue = $this->book->getFrontCatalogue($book->id);
         $this->display();
     }
 
@@ -88,33 +89,21 @@ class book extends control
      */
     public function read($articleID)
     { 
-        $article  = $this->loadModel('article')->getByID($articleID);
+        $article = $this->book->getByID($articleID);
+        $parent  = $this->book->getByID($article->parent);
         
-        /* fetch first category for display. */
-        $category = array_slice($article->categories, 0, 1);
-        $category = $category[0];
-        $category = $this->loadModel('tree')->getById($category->id);
-
-        $type = str_replace('book_', '', $this->dao->findById($category->id)->from(TABLE_CATEGORY)->fetch('type'));
-        $book = $this->loadModel('setting')->getItem("owner=system&module=common&section=book&key=$type");
-        $book = json_decode($book);
-        
-        $category->book = $book->name;
-        $category->code = $type;
-
         $this->createContentNav($article->content);
 
         $this->view->title    = $article->title;
-        $this->view->keywords = trim($article->keywords . ' ' . $category->keyword . ' ' . $this->config->site->keywords);
-        $this->view->desc     = trim($article->summary . ' ' . preg_replace('/<[a-z\/]+.*>/Ui', '', $category->desc));
+        $this->view->keywords = trim($article->keywords . ' ' . $parent->keyword . ' ' . $this->config->site->keywords);
+        $this->view->desc     = trim($article->summary . ' ' . preg_replace('/<[a-z\/]+.*>/Ui', '', $parent->desc));
 
-        $this->view->type        = $type;
         $this->view->article     = $article;
-        $this->view->prevAndNext = $this->loadModel('article')->getPrevAndNext($article->id, $category->id);
+        $this->view->prevAndNext = $this->book->getPrevAndNext($article->id, $parent->id);
         //$this->view->layouts   = $this->loadModel('block')->getLayouts('book.read');
-        $this->view->category    = $category;
+        $this->view->parent      = $parent;
 
-        $this->dao->update(TABLE_ARTICLE)->set('views = views + 1')->where('id')->eq($articleID)->exec(false);
+        $this->dao->update(TABLE_BOOK)->set('views = views + 1')->where('id')->eq($articleID)->exec(false);
 
         $this->display();
     }
@@ -143,8 +132,11 @@ class book extends control
 
         if($_POST)
         {
-            $bookID = $this->book->create($parent);
-            $locate = $this->inlink('admin', "bookID=$parent");
+            $bookID  = $this->book->create($parent);
+            $parent  = $this->book->getByID($parent);
+            $origins = explode(',', $parent->path);
+            $origin  = $origins[1];
+            $locate  = $this->inlink('admin', "bookID=$origin");
             if($parent == 0) $locate = $this->inlink('admin', "bookID=$bookID");
             if($bookID) $this->send(array('result' => 'success', 'message'=>$this->lang->saveSuccess, 'locate' => $locate));
             $this->send(array('result' => 'fail', 'message' => dao::getError()));
@@ -165,9 +157,10 @@ class book extends control
      */
     public function edit($bookID)
     {
-        $book = $this->book->getByID($bookID);
-
-        $parent = $this->book->getByID($book->parent);
+        $book    = $this->book->getByID($bookID);
+        $parent  = $this->book->getByID($book->parent);
+        $origins = explode(',', $book->path);
+        $origin  = $origins[1];
 
         if($_POST)
         {
@@ -177,7 +170,7 @@ class book extends control
         }
 
         $this->view->title   = $this->lang->book->edit;
-        $this->view->parents = $this->book->getOptionMenu(0, $removeRoot = false);
+        $this->view->parents = $this->book->getOptionMenu($origin, $removeRoot = true);
         $this->view->book    = $book;
         $this->view->parent  = $parent;
         $this->display();
@@ -219,5 +212,61 @@ class book extends control
             $nav .= "</div>";
             $content = $nav . $content;
         }
+    }
+
+    /**
+     * sort up.
+     * 
+     * @param  int    $id 
+     * @access public
+     * @return string;
+     */
+    public function up($id)
+    {
+        $book = $this->book->getByID($id);
+        $prev = $this->dao->select('id, `order`')
+            ->from(TABLE_BOOK)
+            ->where('parent')->eq($book->parent)
+            ->andWhere('`order`')->lt($book->order)
+            ->orderBy('`order` desc')
+            ->limit(1)
+            ->fetch();
+        if(!$prev) return false;
+
+        $order = $prev->order;
+
+        $this->dao->update(TABLE_BOOK)->set('`order`')->eq($book->order)->where('id')->eq($prev->id)->exec();
+        $this->dao->update(TABLE_BOOK)->set('`order`')->eq($order)->where('id')->eq($book->id)->exec();
+
+        if(dao::isError()) $this->send(array('result' => 'fail', 'message' => dao::getError()));
+        $this->send(array('result' => 'success'));
+    }
+
+    /**
+     * sort down.
+     * 
+     * @param  int    $id 
+     * @access public
+     * @return string;
+     */
+    public function down($id)
+    {
+        $book = $this->book->getByID($id);
+        $next = $this->dao->select('id, `order`')
+            ->from(TABLE_BOOK)
+            ->where('parent')->eq($book->parent)
+            ->andWhere('`order`')->gt($book->order)
+            ->orderBy('`order`')
+            ->limit(1)
+            ->fetch();
+        if(!$next) return false;
+
+        $order = $next->order;
+
+        $this->dao->update(TABLE_BOOK)->set('`order`')->eq($book->order)->where('id')->eq($next->id)->exec();
+        $this->dao->update(TABLE_BOOK)->set('`order`')->eq($order)->where('id')->eq($book->id)->exec();
+
+        if(dao::isError()) $this->send(array('result' => 'fail', 'message' => dao::getError()));
+        $this->send(array('result' => 'success'));
     }
 }
