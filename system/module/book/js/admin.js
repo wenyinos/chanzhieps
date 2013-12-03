3cc7c66036d9d7d27f84bb84dd8ad61e531957c9
daitingting <daitingting@xirangit.com>
2013-12-03 17:36:12 +0800

    * add js for sort.

--- system/module/book/js/admin.js
+++ system/module/book/js/admin.js
@@ -10,5 +10,22 @@ $(document).ready(function()
             $('.leftmenu a[href*=' + bookID + ']').parent().addClass('active');
         })
     }
+
+    $('.sort').click(function()
+    {
+        $.getJSON($(this).attr('href'), function(data) 
+        {
+            if(data.result=='success')
+            {
+                location.reload();
+            }
+            else
+            {
+                alert(data.message);
+            }
+        });
+
+        return false;
+    });
 });
 
