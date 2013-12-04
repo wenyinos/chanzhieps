18eb26274dbd181f26ae96d30be301b791c79cf5
wyd621 <wyd621@163.com>
2013-12-03 18:19:32 -0800

    * adjust for task #438.

--- system/module/article/js/create.js
+++ system/module/article/js/create.js
@@ -7,12 +7,17 @@ $('.draft').click(function()
 {
     $(this).parent().find('#status').remove();
     $(this).after("<input type='hidden' name='status' id='status' value='draft' />");
+    $('#submit').attr('type', 'button');
+    $(this).attr('type', 'submit');
     $('#ajaxForm').submit();
+    return false;
 });
 
 $("#submit").click(function()
 {
     $(this).parent().find('#status').remove();
+    $('.draft').attr('type', 'button');
+    $(this).attr('type', 'submit');
     $('#ajaxForm').submit();
     return false;
 });
