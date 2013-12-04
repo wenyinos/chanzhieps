d5ce15e59c45bf0085760ae43d0a7fc8623f14f7
daitingting <daitingting@xirangit.com>
2013-12-04 17:40:11 +0800

    * adjust code.

--- system/module/book/view/admin.html.php
+++ system/module/book/view/admin.html.php
@@ -4,7 +4,7 @@
  *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL
- * @author      Tingtin Dai<daitingting@xirangit.com>
+ * @author      Tingting Dai<daitingting@xirangit.com>
  * @package     book
  * @version     $Id$
  * @link        http://www.chanzhi.org
