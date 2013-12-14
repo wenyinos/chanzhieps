1fffb55e164b9f41df58a0d55e577b4f33414b85
xia0ta0 <xia0ta0@outlook.com>
2013-12-13 18:42:46 -0800

    * fix bugs: isset $_SESSION['album'], allowedTags.

--- system/module/reply/config.php
+++ system/module/reply/config.php
@@ -1,4 +1,3 @@
 <?php
 $config->reply->editor = new stdclass();
 $config->reply->editor->edit = array('id' => 'content', 'tools' => 'simpleTools');
-$config->reply->editor->allowedTags = '<p><span><h1><h2><h3><h4><em><u><strong><br><ol><ul><li><img><a><b>';
