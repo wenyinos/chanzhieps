2d0233960304bdc7481df7b5fb1d6b7076dd37d0
xia0ta0 <xia0ta0@outlook.com>
2013-12-13 19:05:55 -0800

    * fix bug: undefined 'thread';

--- system/module/thread/model.php
+++ system/module/thread/model.php
@@ -249,6 +249,7 @@ class threadModel extends model
         if(dao::isError()) return false;
 
         /* Update board stats. */
+        $thread = $this->getByID($threadID);
         $this->loadModel('forum')->updateBoardStats($thread->board);
         return !dao::isError();
     }
@@ -263,6 +264,11 @@ class threadModel extends model
     public function show($threadID)
     {
         $this->dao->update(TABLE_THREAD)->set('hidden')->eq(0)->where('id')->eq($threadID)->exec();
+        if(dao::isError()) return false;
+
+        /* Update board stats. */
+        $thread = $this->getByID($threadID);
+        $this->loadModel('forum')->updateBoardStats($thread->board);
         return !dao::isError();
     }
 
