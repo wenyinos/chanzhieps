9a406c0b13b1c52eec9606e2920dade84c78765d
Catouse <catouse@gmail.com>
2013-12-18 10:30:54 +0800

    * removed useless css code.

--- system/module/index/view/index.html.php
+++ system/module/index/view/index.html.php
@@ -2,6 +2,6 @@
 <?php include '../../common/view/treeview.html.php';?>
 
 <?php $this->block->printRegion($layouts, 'index_index', 'header');?>
-<div class='row'><?php $this->block->printRegion($layouts, 'index_index', 'bottom', "<div class='col-md-4'>", '</div>');?></div>
+<div class='row' id='focus'><?php $this->block->printRegion($layouts, 'index_index', 'bottom', "<div class='col-md-4'>", '</div>');?></div>
 <?php $this->block->printRegion($layouts, 'index_index', 'footer');?>
 <?php include '../../common/view/footer.html.php';?>
