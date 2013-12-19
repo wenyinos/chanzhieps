da2bd66f79e608427f2a48a21825b33713f65f3d
daitingting <daitingting@xirangit.com>
2013-12-19 14:51:59 +0800

    * adjust code.

--- system/module/page/view/view.html.php
+++ system/module/page/view/view.html.php
@@ -11,7 +11,7 @@ include '../../common/view/treeview.html.php';
         <p><?php echo $page->content;?></p>
         <div class='article-file mt-10px'><?php $this->loadModel('article')->printFiles($page->files);?></div>
         <?php if($page->keywords):?>
-        <div class='keywords'><strong><?php echo $lang->page->keywords;?></strong><?php echo $lang->colon . $page->keywords;?></div>
+        <div class='keywords'><strong><?php echo $this->lang->article->keywords;?></strong><?php echo $lang->colon . $page->keywords;?></div>
         <?php endif;?>
       </div>
     </div>
