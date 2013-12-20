7f5ef7e4c2333b22ab79eed9d1212051fc2f5fe5
Catouse <catouse@gmail.com>
2013-12-20 20:13:13 +0800

    * ajusted views of forum module.

--- system/module/forum/view/index.html.php
+++ system/module/forum/view/index.html.php
@@ -1,16 +1,16 @@
 <?php include '../../common/view/header.html.php'; ?>
 <?php $this->block->printRegion($layouts, 'forum_index', 'header');?>
 <?php $common->printPositionBar();?>
-<div id="boards">
-<?php foreach($boards as $parentBoard):?>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4><i class="icon-comments icon-large"></i> <?php echo $parentBoard->name;?></h4>
-    </div>
-    <div class="panel-body no-padding">
+<div class='page-wrapper'>
+  <div id='boards'>
+  <?php foreach($boards as $parentBoard):?>
+    <div class='panel'>
+      <div class='panel-heading'>
+        <strong><i class='icon-comments icon-large'></i> <?php echo $parentBoard->name;?></strong>
+      </div>
       <table class='table table-hover table-striped'>
         <thead>
-          <tr class='a-center'>
+          <tr class='text-center'>
             <th colspan='2'>&nbsp;<?php echo $lang->forum->board;?></th>
             <th><?php echo $lang->forum->owners;?></th>
             <th><?php echo $lang->forum->threadCount;?></th>
@@ -20,31 +20,32 @@
         </thead>
         <tbody>
           <?php foreach($parentBoard->children as $childBoard):?>
-          <tr valign='middle' class='a-center'>
-            <td class='w-20px'><?php echo $this->forum->isNew($childBoard) ? "<span class='new-board'><i class='icon-comment icon-large'></i></span>" : "<span class='common-board'><i class='icon-comment icon-large'></i></span>"; ?></td>
-            <td class='a-left'>
+          <tr class='text-center text-middle'>
+            <td style='width: 20px'><?php echo $this->forum->isNew($childBoard) ? "<span class='text-success'><i class='icon-comment icon-large'></i></span>" : "<span class='text-muted'><i class='icon-comment icon-large'></i></span>"; ?></td>
+            <td class='text-left'>
               <strong><?php echo html::a(inlink('board', "id=$childBoard->id", "category={$childBoard->alias}"), $childBoard->name);?></strong><br />
-              <i><?php echo $childBoard->desc;?></i>
+              <small class='text-muted'><?php echo $childBoard->desc;?></small>
             </td>
-            <td class='w-50px strong'><nobr><?php echo trim($childBoard->moderators, ',');?></nobr></td>
-            <td class='w-70px'><?php echo $childBoard->threads;?></td>
-            <td class='w-70px'><?php echo $childBoard->posts;?></td>
-            <td class='w-150px a-left'>
+            <td style='width: 50px'><strong><nobr><?php echo trim($childBoard->moderators, ',');?></nobr></strong></td>
+            <td style='width: 70px'><?php echo $childBoard->threads;?></td>
+            <td style='width: 70px'><?php echo $childBoard->posts;?></td>
+            <td style='width: 150px' class='text-left'>
               <?php
               if($childBoard->postedBy)
               {
-                  echo substr($childBoard->postedDate, 5, -3) . ' '; 
+                  echo substr($childBoard->postedDate, 5, -3) . '<br/>'; 
                   echo html::a($this->createLink('thread', 'locate', "threadID={$childBoard->postID}&replyID={$childBoard->replyID}"), $childBoard->postedBy);;
               }
               ?>
             </td>
-          </tr>  
+          </tr>
           <?php endforeach;?>
         </tbody>
-      </table>    
+      </table>
     </div>
+  <?php endforeach;?>
   </div>
-<?php endforeach;?>   
 </div>
+
 <?php $this->block->printRegion($layouts, 'forum_index', 'footer');?>
 <?php include '../../common/view/footer.html.php'; ?>
