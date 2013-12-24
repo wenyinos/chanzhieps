32b645c51115c319c674b8af8117c3b328dec51f
daitingting <daitingting@xirangit.com>
2013-12-24 15:39:18 +0800

    * fix bugs.

--- system/module/tag/lang/en.php
+++ system/module/tag/lang/en.php
@@ -6,3 +6,4 @@ $lang->tag->link   = 'link';
 $lang->tag->admin     = 'Manage keywords';
 $lang->tag->editLink  = 'Edit Link';
 $lang->tag->inputLink = 'Please input link';
+$lang->tag->inputTag  = 'Please input tag';
--- system/module/tag/lang/zh-cn.php
+++ system/module/tag/lang/zh-cn.php
@@ -6,3 +6,4 @@ $lang->tag->link   = '链接';
 $lang->tag->admin     = '关键词管理';
 $lang->tag->editLink  = '编辑链接';
 $lang->tag->inputLink = '请输入链接';
+$lang->tag->inputTag  = '请输入关键词';
