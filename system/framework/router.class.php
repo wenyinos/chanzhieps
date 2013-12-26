d0fb9e3ad16ba2b0d1e7862a3efb0ac6ed6e76a6
guanxiying <guanxiying@xirangit.com>
2013-12-26 02:00:50 +0000

    * trim wwwRoot from errorlog.

--- system/framework/router.class.php
+++ system/framework/router.class.php
@@ -1544,6 +1544,7 @@ class router
         if(!defined('IN_SHELL') and !($this->server->server_addr == '127.0.0.1' or filter_var($this->server->server_addr, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE) === false))
         {
             $errorLog  = str_replace($this->getBasePath(), '', $errorLog);
+            $errorLog  = str_replace($this->wwwRoot, '', $errorLog);
         }
 
         /* Save to log file. */
