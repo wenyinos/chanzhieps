967737aed21f353d8bce2a359770f03df7d81a23
daitingting <daitingting@xirangit.com>
2013-12-26 14:40:08 +0800

    * adjust code.

--- system/module/slide/view/create.html.php
+++ system/module/slide/view/create.html.php
@@ -19,11 +19,11 @@
       <table class='table table-form'>
         <tr>
           <th class='col-md-1 col-xs-2'><?php echo $lang->slide->title;?></th>
-          <td class='col-md-5 col-xs-7'><?php echo html::input('title', '', 'class="form-control"');?></td><td></td>
+          <td class='col-md-5 col-xs-7'><?php echo html::input('title', '', "class='form-control'");?></td><td></td>
         </tr>
         <tr>
           <th><?php echo $lang->slide->url;?></th>
-          <td><?php echo html::input('url', '', 'class="form-control"');?></td><td></td>
+          <td><?php echo html::input('url', '', "class='form-control'");?></td><td></td>
         </tr>
         <tr>
           <th><?php echo $lang->slide->image;?></th>
@@ -32,7 +32,7 @@
         </tr>
         <tr>
           <th><?php echo $lang->slide->label;?></th>
-          <td><?php echo html::input('label', '', 'class="form-control"');?></td><td></td>
+          <td><?php echo html::input('label', '', "class='form-control'");?></td><td></td>
         </tr>
         <tr>
           <th><?php echo $lang->slide->summary;?></th>
