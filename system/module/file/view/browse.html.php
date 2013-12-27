f467ab8aa99955a634a9618be7f9689731c82fd5
guanxiying <guanxiying@xirangit.com>
2013-12-27 05:11:16 +0000

    * mv jscodes from view to js file.

--- system/module/file/view/browse.html.php
+++ system/module/file/view/browse.html.php
@@ -75,33 +75,4 @@
     </div>
   </div>
 </div>
-<script>
-$(document).ready(function()
-{   
-    $.setAjaxForm('#fileForm', function(data)
-    {
-        if(data.result == 'success') $.reloadAjaxModal(1500);
-    }); 
-    $.setAjaxLoader('.edit', '#ajaxModal');
-    $('a.option').click(function(data)
-    {
-        $.getJSON($(this).attr('href'), function(data) 
-        {
-            if(data.result == 'success')
-            {
-                $.reloadAjaxModal();
-            }
-            else
-            {
-                alert(data.message);
-            }
-        });
-        return false;
-    });
-
-    $(".modal-backdrop").click(function()
-    {
-        $('.modal').modal('hide');
-    });
-});
-</script>
+<?php js::execute($pageJS);?>
