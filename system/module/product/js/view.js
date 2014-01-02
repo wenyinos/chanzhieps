8b6f10e36d03b1309a957ca74351de2e95a449b5
Catouse <catouse@gmail.com>
2014-01-02 17:00:26 +0800

    * ajusted style of product images.

--- system/module/product/js/view.js
+++ system/module/product/js/view.js
@@ -1,8 +1,8 @@
 $(document).ready(function()
 {
-   	$('.little-image img').click(function()
+   	$('.little-image').mouseover(function()
     {
-        $('.product-image.media-wrapper img').attr('src', $(this).attr('src').replace('s_', 'm_'));
+        $('.product-image.media-wrapper img').attr('src', $(this).find('img').attr('src').replace('s_', 'm_'));
         return false;
     });
 
