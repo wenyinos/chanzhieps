54cdadc685ec64125363e2186fa0bddae80ad84b
daitingting <daitingting@xirangit.com>
2014-01-06 10:21:27 +0800

    * adjust the style of code and html file.

--- system/module/block/view/block/code.html.php
+++ system/module/block/view/block/code.html.php
@@ -2,7 +2,7 @@
 /**
  * The code block view file of block module of chanzhiEPS.
  *
- * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @copyright   Copyright 2013-2013 闈掑矝鎭￥缃戠粶淇℃伅鏈夐檺鍏徃 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
  * @author      Xiying Guan <guanxiying@xirangit.com>
  * @package     block
@@ -10,4 +10,11 @@
  * @link        http://www.chanzhi.org
 */
 ?>
-<?php echo $block->content;?>
+<div id='code' class='panel panel-block'>
+  <div class='panel-heading'>
+    <h4><i class='icon-group'></i> <?php echo $block->title;?></h4>
+  </div>
+  <div class='panel-body'>
+    <?php echo $block->content;?>
+  </div>
+</div>
