c639a7b50a11e61df6dad3d2cbb16f01dab5af88
daitingting <daitingting@xirangit.com>
2014-01-06 17:31:45 +0800

    * add judge if book is empty.

--- system/module/book/view/browse.html.php
+++ system/module/book/view/browse.html.php
@@ -1,14 +1,17 @@
 <?php include '../../common/view/header.html.php'; ?>
-<?php $common->printPositionBar($node->origins);?>
+<?php if(isset($node)) $common->printPositionBar($node->origins);?>
 <div class='row'>
   <div class='col-md-3'>
     <nav class='booksNav'>
       <ul class='nav nav-primary nav-stacked'>
         <li class='nav-heading'><?php echo $lang->book->list;?></li>
         <?php
-        foreach($books as $menu)
+        if(!empty($books))
         {
-            echo '<li' . (($menu->title == $book->title) ? " class='active'" : '') . '>' . html::a(inlink('browse', "bookID=$menu->id", "book=$menu->alias"), '<i class="icon-book"></i> &nbsp;' . $menu->title) . '</li>';
+            foreach($books as $menu)
+            {
+                echo '<li' . (($menu->title == $book->title) ? " class='active'" : '') . '>' . html::a(inlink('browse', "bookID=$menu->id", "book=$menu->alias"), '<i class="icon-book"></i> &nbsp;' . $menu->title) . '</li>';
+            }
         }
         ?>
       </ul>
@@ -18,13 +21,13 @@
 
   <div class='col-md-9'>
     <div class='panel panel-block'>
-      <?php if($book->title): ?>
+      <?php if(!empty($book->title)):?>
       <div class='panel-heading'>
         <h4 class='title'><?php echo $book->title;?></h4>
       </div>
       <?php endif; ?>
       <div class='panel-body'>
-        <div  class='books'><?php echo $catalog;?></div>
+        <div  class='books'><?php if(!empty($catalog)) echo $catalog;?></div>
       </div>
     </div>
   </div>
