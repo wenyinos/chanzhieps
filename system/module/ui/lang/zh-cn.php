513653cbaa1266e68a52a8aee0563c7ea7d1d7aa
Catouse <catouse@gmail.com>
2014-01-07 16:47:08 +0800

    * removed box-shadow of drowdown menu.

--- system/module/ui/lang/zh-cn.php
+++ system/module/ui/lang/zh-cn.php
@@ -31,4 +31,5 @@ $lang->ui->themes['tree']       = '蝉之树';
 $lang->ui->themes['brightdark'] = '蝉憩';
 $lang->ui->themes['tartan']     = '蝉之格';
 $lang->ui->themes['blue']       = '蓝';
+$lang->ui->themes['wide']       = '宽屏';
 $lang->ui->changetheme = '点击来使用此主题';
