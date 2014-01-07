8b5bb56a2ac3023ff86bcb74fad0cc312174a954
guanxiying <guanxiying@xirangit.com>
2014-01-07 01:20:52 +0000

    * fix js bug when lack of panel.

--- system/module/index/js/index.js
+++ system/module/index/js/index.js
@@ -1,22 +1,27 @@
 $(document).ready(function()
 {          
-    /* Set rows label of bottom blocks. */
-    $('.panel').each(function(index)
-    {
-        row = parseInt(parseInt(index) / 3);
-        $(this).addClass('row-' + row);
-    });
-    rows = row;
-
-    // auto ajust panel height.
-    for(i=0; i<=rows; i++)
-    {
-        fitPanelHeight = 0;
-        $(".row-" + i ).each(function(){fitPanelHeight=Math.max($(this).height(),fitPanelHeight);}).height(fitPanelHeight);
-    }
 
     // add "index" class to the body element.
     $('body').addClass('index');
 
     $('.nav-system-home').addClass('active');
+
+    if($('.panel').size())
+    {
+        /* Set rows label of bottom blocks. */
+        $('.panel').each(function(index)
+        {
+            row = parseInt(parseInt(index) / 3);
+            $(this).addClass('row-' + row);
+        });
+
+        rows = row;
+
+        // auto ajust panel height.
+        for(i=0; i<=rows; i++)
+        {
+            fitPanelHeight = 0;
+            $(".row-" + i ).each(function(){fitPanelHeight=Math.max($(this).height(),fitPanelHeight);}).height(fitPanelHeight);
+        }
+    }
 })
