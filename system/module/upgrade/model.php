6aff374ce8a556aa2c128ac8fac7220478ba1e8e
guanxiying <guanxiying@xirangit.com>
2014-01-08 06:05:39 +0000

    * remove . from unify lables and remove skip param.

--- system/module/upgrade/model.php
+++ system/module/upgrade/model.php
@@ -125,7 +125,7 @@ class upgradeModel extends model
         $articles = $this->dao->select('id, keywords')->from(TABLE_ARTICLE)->fetchPairs('id', 'keywords');  
         foreach($articles as $id => $keywords)
         {
-            $keywords = seo::unify($keywords, ',', '.');
+            $keywords = seo::unify($keywords, ',');
             $this->dao->update(TABLE_ARTICLE)->set('keywords')->eq($keywords)->where('id')->eq($id)->exec();
             $tags = $keywords;
         }
@@ -133,7 +133,7 @@ class upgradeModel extends model
         $products = $this->dao->select('id, keywords')->from(TABLE_PRODUCT)->fetchPairs('id', 'keywords');  
         foreach($products as $id => $keywords)
         {
-            $keywords = seo::unify($keywords, ',', '.');
+            $keywords = seo::unify($keywords, ',');
             $this->dao->update(TABLE_PRODUCT)->set('keywords')->eq($keywords)->where('id')->eq($id)->exec();
             $tags .= ',' . $keywords;
         }
@@ -141,7 +141,7 @@ class upgradeModel extends model
         $categories = $this->dao->select('id, keywords')->from(TABLE_CATEGORY)->fetchPairs('id', 'keywords');  
         foreach($categories as $id => $keywords)
         {
-            $keywords = seo::unify($keywords, ',', '.');
+            $keywords = seo::unify($keywords, ',');
             $this->dao->update(TABLE_CATEGORY)->set('keywords')->eq($keywords)->where('id')->eq($id)->exec();
             $tags .= ',' . $keywords;
         }
