43089a7a342213f4af29f3e9e2cd28fb340f0127
guanxiying <guanxiying@xirangit.com>
2014-01-11 05:32:02 +0000

    * fix call of user->account bug.

--- system/module/user/view/changepassword.html.php
+++ system/module/user/view/changepassword.html.php
@@ -9,7 +9,7 @@
         <table class='table table-form' style="border:none;">
           <tr>
             <th class="col-xs-4"><?php echo $lang->user->account;?></th>
-            <td class="col-xs-6"><?php echo $user->account;?></td><td></td>
+            <td class="col-xs-6"><?php echo $this->app->user->account;?></td><td></td>
           </tr>  
           <tr>
             <th><?php echo $lang->user->newPassword;?></th>
