6e3c1719811f96e7c8df44a6a40fedbc3a52a465
Catouse <catouse@gmail.com>
2014-02-26 17:27:57 +0800

    * ajusted layout of company index view.

--- system/module/company/view/index.html.php
+++ system/module/company/view/index.html.php
@@ -2,20 +2,30 @@
 include '../../common/view/header.html.php';
 ?>
 <?php $common->printPositionBar($this->app->getModuleName());?>
-<div class='panel'>
-  <div class='panel-heading'><strong><i class='icon-group'></i> <?php echo $lang->aboutUs; ?></strong></div>
-  <div class='panel-body'>
-    <div class='article-content'>
-      <?php echo $company->content;?>
-      <div id='contact'>
+<div class="row">
+  <div class="col-md-8">
+    <div class='panel'>
+      <div class='panel-heading'><strong><i class='icon-group'></i> <?php echo $lang->aboutUs; ?></strong></div>
+      <div class='panel-body'>
+        <?php echo $company->content;?>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-4">
+    <div class="panel">
+      <div class="panel-heading"><strong><i class="icon-phone"></i> <?php echo $lang->company->contact;?></strong></div>
+      <div class="panel-body">
+        <table class='table table-data'>
         <?php foreach($contact as $item => $value):?>
-        <dl>
-          <dt><?php echo $lang->company->$item;?>:</dt>
-          <dd><?php echo $value;?></dd>
-        </dl>
+          <tr>
+            <th><?php echo $lang->company->$item;?>:</th>
+            <td><?php echo $value;?></td>
+          </tr>
         <?php endforeach;?>
+        </table>
       </div>
     </div>
   </div>
 </div>
+
 <?php include '../../common/view/footer.html.php'; ?>
