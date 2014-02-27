90861f3af66501a159ee97c8cc22cc05fb338056
daitingting <daitingting@xirangit.com>
2014-02-26 16:30:26 -0800

    * add addedDate.

--- system/module/book/view/catalog.html.php
+++ system/module/book/view/catalog.html.php
@@ -11,6 +11,7 @@
  */
 ?>
 <?php include '../../common/view/header.admin.html.php'; ?>
+<?php include '../../common/view/datepicker.html.php';?>
 <?php js::set('path', $node ? explode(',', $node->path) : array(0));?>
 <form id='ajaxForm' method='post'>
 <div class='panel'>
@@ -21,8 +22,9 @@
         <th class='w-p10'><?php echo $lang->book->type;?></th>
         <th class='w-p10'><?php echo $lang->book->author;?></th>
         <th><?php echo $lang->book->title;?></th>
-        <th class='w-p30'><?php echo $lang->book->alias;?></th>
+        <th class='w-300px'><?php echo $lang->book->alias;?></th>
         <th class='w-p10'><?php echo $lang->book->keywords;?></th>
+        <th class='w-180px'><?php echo $lang->book->addedDate;?></th>
         <th class='w-80px'><?php echo $lang->actions; ?></th>
       </tr>
     </thead>
@@ -30,11 +32,12 @@
     <tbody>
       <?php foreach($children as $child):?>
       <tr class='text-center text-middle'>
-        <td><?php echo html::select("type[$child->id]",    $lang->book->typeList, $child->type, "class='form-control'");?></td>
-        <td><?php echo html::input("author[$child->id]",   $child->author,   "class='form-control'");?></td>
-        <td><?php echo html::input("title[$child->id]",    $child->title,    "class='form-control'");?></td>
-        <td><?php echo html::input("alias[$child->id]",    $child->alias,    "class='form-control'");?></td>
-        <td><?php echo html::input("keywords[$child->id]", $child->keywords, "class='form-control'");?></td>
+        <td><?php echo html::select("type[$child->id]",     $lang->book->typeList, $child->type, "class='form-control'");?></td>
+        <td><?php echo html::input("author[$child->id]",    $child->author,    "class='form-control'");?></td>
+        <td><?php echo html::input("title[$child->id]",     $child->title,     "class='form-control'");?></td>
+        <td><?php echo html::input("alias[$child->id]",     $child->alias,     "class='form-control'");?></td>
+        <td><?php echo html::input("keywords[$child->id]",  $child->keywords,  "class='form-control'");?></td>
+        <td><?php echo html::input("addedDate[$child->id]", $child->addedDate, "class='form-control date'");?></td>
         <td>
           <?php echo html::hidden("order[$child->id]", $child->order, "class='order'");?>
           <?php echo html::hidden("mode[$child->id]", 'update');?>
@@ -50,6 +53,7 @@
         <td><?php echo html::input("title[]", '', "class='form-control'");?></td>
         <td><?php echo html::input("alias[]", '', "class='form-control' placeholder='{$lang->alias}'");?></td>
         <td><?php echo html::input("keywords[]", '', "class='form-control'");?></td>
+        <td><?php echo html::input("addedDate[]", helper::now(), "class='form-control date'");?></td>
         <td>
           <?php echo html::hidden("order[]", '', "class='order'");?>
           <?php echo html::hidden("mode[]", 'new');?>
