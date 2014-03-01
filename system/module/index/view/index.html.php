0f21f26668d3879d5aed3ae32b2fa6a08b1e4271
daitingting <daitingting@xirangit.com>
2014-02-28 18:08:01 -0800

    * adjust code.

--- system/module/index/view/index.html.php
+++ system/module/index/view/index.html.php
@@ -2,8 +2,8 @@
 <?php include '../../common/view/treeview.html.php';?>
 
 <div id='focus' class='block-list'>
-  <div class='row' data-default-grid='12'><?php $this->block->printRegion($layouts, 'index_index', 'header', ture);?></div>
-  <div class='row' data-default-grid='4'><?php $this->block->printRegion($layouts, 'index_index', 'middle', ture);?></div>
-  <div class='row' data-default-grid='6'><?php $this->block->printRegion($layouts, 'index_index', 'footer', ture);?></div>
+  <div class='row' data-default-grid='12'><?php $this->block->printRegion($layouts, 'index_index', 'header', true);?></div>
+  <div class='row' data-default-grid='4'><?php $this->block->printRegion($layouts, 'index_index', 'middle', true);?></div>
+  <div class='row' data-default-grid='6'><?php $this->block->printRegion($layouts, 'index_index', 'footer', true);?></div>
 </div>
 <?php include '../../common/view/footer.html.php';?>
