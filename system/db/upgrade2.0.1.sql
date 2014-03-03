62f8dea8f9d051d14a8af4c7b205503f99643b46
daitingting <daitingting@xirangit.com>
2014-03-02 18:07:27 -0800

    * alert resetKey field to reset.

--- system/db/upgrade2.0.1.sql
+++ system/db/upgrade2.0.1.sql
@@ -3,3 +3,4 @@ ALTER TABLE `eps_file` ADD `width` smallint unsigned NOT NULL AFTER `size`,
 ADD `height` smallint unsigned NOT NULL AFTER `width`;
 
 ALTER TABLE `eps_message` ADD `account` char(30) COLLATE 'utf8_general_ci' NOT NULL AFTER `objectID`;
+ALTER TABLE eps_user CHANGE resetKey reset char(64) NOT NULL;
