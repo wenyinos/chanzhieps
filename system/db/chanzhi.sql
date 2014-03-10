f33c30f6c9b6c1d5086f525534f753c8053508b3
guanxiying <guanxiying@xirangit.com>
2014-03-09 23:55:16 -0700

    * Add weichat tables.

--- system/db/chanzhi.sql
+++ system/db/chanzhi.sql
@@ -295,6 +295,49 @@ CREATE TABLE IF NOT EXISTS `eps_user` (
   KEY `account` (`account`,`password`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
+-- DROP TABLE IF EXISTS `eps_wx_public`;
+CREATE TABLE IF NOT EXISTS `eps_wx_public` (
+  `id`        smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `account`   varchar(20) NOT NULL,
+  `appID`     char(30) NOT NULL,
+  `appSecret` char(32) NOT NULL,
+  `url`       varchar(100) NOT NULL,
+  `token`     varchar(100) NOT NULL,
+  `desc`      varchar(255) NOT NULL,
+  `type`      enum('subscribe', 'service') NOT NULL,
+  `addedDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `eps_wx_response`;
+CREATE TABLE IF NOT EXISTS `eps_wx_response` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `public` smallint(3) NOT NULL,
+  `key` varchar(100) NOT NULL,
+  `group` varchar(100) NOT NULL,
+  `type` enum('text', 'rich', 'link') NOT NULL, 
+  `source` enum('system', 'manual') NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- DROP TABLE IF EXISTS `eps_wx_message`;
+CREATE TABLE IF NOT EXISTS `eps_wx_message` (
+  `id` mediumint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `public` smallint(3) NOT NULL,
+  `wid`      char(64) NOT NULL, 
+  `rid`      char(64) NOT NULL, 
+  `replyer`  varchar(30) NOT NULL,
+  `to`       varchar(50) NOT NULL,
+  `from`     varchar(50) NOT NULL,
+  `response` smallint(5) NOT NULL,
+  `content`  text NOT NULL,
+  `type`     char(30) NOT NULL,
+  `status`   enum('wait', 'replied'),
+  `time`     datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 -- Insert data into `eps_layout`;
 INSERT INTO `eps_layout` (`page`, `region`, `blocks`) VALUES
 ('index_index', 'header', '[{"id":"5","grid":"","titleless":0,"borderless":0}]'),
---
+++ system/db/upgrade2.1.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS `eps_wx_public` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `account` varchar(20) NOT NULL,
+  `appID` char(30) NOT NULL,
+  `appSecret` char(32) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `desc` varchar(255) NOT NULL,
+  `type` enum('subscribe', 'service') NOT NULL,
+  `addedDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `eps_wx_response` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `public` smallint(3) NOT NULL,
+  `key` varchar(100) NOT NULL,
+  `group` varchar(100) NOT NULL,
+  `type` enum('text', 'rich', 'link') NOT NULL, 
+  `source` enum('system', 'manual') NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `eps_wx_message` (
+  `id` mediumint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `public` smallint(3) NOT NULL,
+  `wid`      char(64) NOT NULL, 
+  `rid`      char(64) NOT NULL, 
+  `replyer`  varchar(30) NOT NULL,
+  `to`       varchar(50) NOT NULL,
+  `from`     varchar(50) NOT NULL,
+  `response` smallint(5) NOT NULL,
+  `content`  text NOT NULL,
+  `type`     char(30) NOT NULL,
+  `status`   enum('wait', 'replied'),
+  `time`     datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
