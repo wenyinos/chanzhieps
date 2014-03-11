784d7d9f3971c7215ba69f6ad0683b71faf781d3
daitingting <daitingting@xirangit.com>
2014-03-11 13:30:55 +0800

    * add lang file for weichat module.

---
+++ system/module/weichat/lang/en.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The weichat module zh-cn file of chanzhiEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     weichat
+ * @version     $Id$
+ * @link        http://www.chanzhi.org
+ */
+$lang->weichat->id        = 'ID';
+$lang->weichat->type      = 'Type';
+$lang->weichat->name      = 'Name';
+$lang->weichat->account   = 'Account';
+$lang->weichat->appID     = 'appID';
+$lang->weichat->appSecret = 'appSecret';
+$lang->weichat->token     = 'Token';
+$lang->weichat->url       = 'URL';
+
+$lang->weichat->create    = 'Create';
+$lang->weichat->admin     = 'Admin';
+$lang->weichat->list      = 'List';
+
+$lang->weichat->typeList['subscribe'] = 'Subscribe';
+$lang->weichat->typeList['service']   = 'Service';
---
+++ system/module/weichat/lang/zh-cn.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The weichat module zh-cn file of chanzhiEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     weichat
+ * @version     $Id$
+ * @link        http://www.chanzhi.org
+ */
+$lang->weichat->id        = '编号';
+$lang->weichat->type      = '类型';
+$lang->weichat->name      = '微信名';
+$lang->weichat->account   = '微信号';
+$lang->weichat->appID     = 'appID';
+$lang->weichat->appSecret = 'appSecret';
+$lang->weichat->token     = 'Token';
+$lang->weichat->url       = 'URL';
+
+$lang->weichat->create    = '添加公众号';
+$lang->weichat->admin     = '维护公众号';
+$lang->weichat->list      = '公众号列表';
+
+$lang->weichat->typeList['subscribe'] = '订阅号';
+$lang->weichat->typeList['service']   = '服务号';
