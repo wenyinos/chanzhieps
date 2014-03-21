c1d453da5b9b9746190bbbff90de968b9a9ea144
daitingting <daitingting@xirangit.com>
2014-03-21 08:27:44 +0800

    * Add modal for set response.

--- system/module/wechat/view/adminresponse.html.php
+++ system/module/wechat/view/adminresponse.html.php
@@ -64,7 +64,7 @@
         </td>
         <td>
           <?php
-          echo html::a($this->createLink('wechat', 'setResponse', "public={$response->public}&group={$response->group}&key=$response->key"), $lang->edit);
+          echo html::a($this->createLink('wechat', 'setResponse', "public={$response->public}&group={$response->group}&key=$response->key"), $lang->edit, "data-toggle='modal'");
           echo html::a($this->createLink('wechat', 'deleteResponse', "responseID=$response->id"), $lang->delete, "class='deleter'");
           ?>
         </td>
