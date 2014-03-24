acceb03a2867957f825a8c14c64b91cc0e5c888a
wwccss <chunsheng@cnezsoft.com>
2014-03-24 13:53:48 +0800

    * deny baidu to auto transform page.

--- system/module/blog/view/header.html.php
+++ system/module/blog/view/header.html.php
@@ -27,6 +27,7 @@ $navs = $this->tree->getChildren(0, 'blog');
   <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Cache-Control" content="no-transform" />
   <?php
   if(!isset($title))    $title    = ''; 
   if(!empty($title))    $title   .= $lang->minus;
