735bcd08e7a335f90a97f320ef6979d94dcebd39
guanxiying <guanxiying@xirangit.com>
2014-05-14 02:59:33 +0000

    * add product custom table.

--- system/db/chanzhi.sql
+++ system/db/chanzhi.sql
@@ -225,6 +225,17 @@ CREATE TABLE IF NOT EXISTS `eps_reply` (
   KEY `author` (`author`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE IF NOT EXISTS `eps_product_custom` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `product` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `label` varchar(100) NOT NULL,
+  `value` varchar(200) NOT NULL,
+  `order` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `label` (`product`,`label`),
+  KEY `product` (`product`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 -- DROP TABLE IF EXISTS `eps_tag`;
 CREATE TABLE IF NOT EXISTS `eps_tag` (
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
