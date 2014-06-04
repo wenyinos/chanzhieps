ea0b1514bf6786893764e5c8d77db2e9da591541
guanxiying <guanxiying@xirangit.com>
2014-06-04 08:09:14 +0000

    * fix copy inmage bug.

--- system/module/book/model.php
+++ system/module/book/model.php
@@ -580,7 +580,7 @@ class bookModel extends model
         if($node->type == 'article')
         {
             $this->loadModel('file')->updateObjectID($this->post->uid, $nodeID, 'book');
-            $this->file->fileManager($this->post->content, $nodeID, 'book');
+            $this->file->copyFromContent($this->post->content, $nodeID, 'book');
             if(dao::isError()) return false;
         }
 
