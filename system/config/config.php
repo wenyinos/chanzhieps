16ffa869a09f55b590efb3db5c2a9946a62f124d
wwccss <chunsheng@cnezsoft.com>
2014-06-06 09:58:31 +0800

    * add header and nav tags.

--- system/config/config.php
+++ system/config/config.php
@@ -34,8 +34,8 @@ $config->sessionVar  = RUN_MODE . 'sid';  // The session var name.
 
 /* Set the allowed tags.  */
 $config->allowedTags = new stdclass();
-$config->allowedTags->front = '<p><span><h1><h2><h3><h4><h5><em><u><strong><br><ol><ul><li><img><a><b><font><hr><pre>';    // For front mode.
-$config->allowedTags->admin = $config->allowedTags->front . '<div><table><td><th><tr><tbody><iframe><embed><style>';              // For admin users.
+$config->allowedTags->front = '<p><span><h1><h2><h3><h4><h5><em><u><strong><br><ol><ul><li><img><a><b><font><hr><pre>';           // For front mode.
+$config->allowedTags->admin = $config->allowedTags->front . '<div><table><td><th><tr><tbody><iframe><embed><style><header><nav>'; // For admin users.
 
 /* Views and themes. */
 $config->views  = ',html,json,xml,'; // Supported view types.
