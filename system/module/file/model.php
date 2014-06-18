e49292700b7660198dbda2f01548082b78210d83
daitingting <daitingting@xirangit.com>
2014-06-18 16:44:42 +0800

    * adjust code.

--- system/module/file/model.php
+++ system/module/file/model.php
@@ -551,6 +551,7 @@ class fileModel extends model
             $data->objectType = $objectType;
             $data->addedBy    = $this->app->user->account;
             $data->addedDate  = helper::now();
+            $data->editor     = 1;
 
             $fileExists = $this->dao->select('count(*) as count')->from(TABLE_FILE)
                 ->where('objectType')->eq($objectType)
