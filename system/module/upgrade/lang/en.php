4accd96e46989afc87ec7871be9c479c506b1520
guanxiying <guanxiying@xirangit.com>
2014-06-23 07:30:34 +0000

    * adjust code select.

--- system/module/upgrade/lang/en.php
+++ system/module/upgrade/lang/en.php
@@ -26,14 +26,14 @@ $lang->upgrade->next          = 'Next';
 $lang->upgrade->setOkFile = <<<EOT
 <div class='alert'>
 <h5>For security reason, please do these steps. </h5>
-<p>Create "<code>%s</code>" file. If this file exists already, reopen it and save again.</p>
+<p>Create "<input value='%s' class='autoSelect' readoly />" file. If this file exists already, reopen it and save again.</p>
 </div>
 EOT;
 
 $lang->upgrade->backupData = <<<EOT
 <pre>
 <strong>Using phpMyAdmin or mysqldump to backup database.</strong>
-<code class='red'>$ mysqldump -u %s</span> -p%s %s > chanzhi.sql</code>
+<textarea class='autoSelect w-300px red' readonly rows='1' > mysqldump -u %s -p%s %s > chanzhi.sql </textarea>
 </pre>
 EOT;
 
