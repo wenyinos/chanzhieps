86fcbc82c54ac16b7fdbb8f26378a017eb51ab1b
daitingting <daitingting@xirangit.com>
2014-07-24 15:58:16 +0800

    * Adjust code format.

--- system/module/block/view/setregion.html.php
+++ system/module/block/view/setregion.html.php
@@ -12,22 +12,22 @@
 ?>
 <?php include '../../common/view/header.modal.html.php';?>
 <?php js::set('key', count($blocks));?>
-  <form id='ajaxForm' action="<?php echo inlink('setregion', "page={$page}&region={$region}");?>" method='post'>
-    <table class='table table-striped table-form'>
-      <thead>
-        <tr>
-          <th class='text-center col-xs-6'><?php echo $lang->block->title;?></th>
-          <th class='text-center col-xs-2'><?php echo $lang->block->grid;?></th>
-          <th class='text-center col-xs-2'><?php echo $lang->actions;?></th>
-        </tr>
-      </thead>
-      <tbody>
-      <?php $key = 0; foreach($blocks as $block){ echo $this->block->createEntry($block, $key); $key ++;  }?>
-      </tbody>
-      <tfoot>
-        <tr><td colspan='3' class='a-center'> <?php echo html::submitButton();?></td></tr>
-      </tfoot>
-    </table>
-  </form>
-  <table class='hide'><tbody id='entry'><?php echo $this->block->createEntry(null, 'key');?></tbody></table>
+<form id='ajaxForm' action="<?php echo inlink('setregion', "page={$page}&region={$region}");?>" method='post'>
+  <table class='table table-striped table-form'>
+    <thead>
+      <tr>
+        <th class='text-center col-xs-6'><?php echo $lang->block->title;?></th>
+        <th class='text-center col-xs-2'><?php echo $lang->block->grid;?></th>
+        <th class='text-center col-xs-2'><?php echo $lang->actions;?></th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php $key = 0; foreach($blocks as $block){ echo $this->block->createEntry($block, $key); $key ++;  }?>
+    </tbody>
+    <tfoot>
+      <tr><td colspan='3' class='a-center'> <?php echo html::submitButton();?></td></tr>
+    </tfoot>
+  </table>
+</form>
+<table class='hide'><tbody id='entry'><?php echo $this->block->createEntry(null, 'key');?></tbody></table>
 <?php include '../../common/view/footer.modal.html.php';?>
