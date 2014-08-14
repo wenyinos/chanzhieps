f8b651f78cedcbce3fdef5cc3cfe9ece662f724f
guanxiying <guanxiying@xirangit.com>
2014-08-14 01:27:20 +0000

    * move tree manage link to treemenu.

--- system/module/common/view/header.admin.html.php
+++ system/module/common/view/header.admin.html.php
@@ -29,8 +29,13 @@
     <?php if($moduleMenu) echo $moduleMenu;?>
     <?php if(!empty($treeModuleMenu)):?>
     <div class='panel category-nav affix hiddden-xs hidden-sm'>
-    <div class='panel-heading'><strong><?php echo $lang->tree->browseByCategory;?></strong></div>
-       <div class='panel-body'><?php echo $treeModuleMenu;?></div>
+      <div class='panel-heading'>
+        <strong><?php echo $lang->tree->browseByCategory;?></strong>
+        <?php if(!empty($treeManageLink)):?>
+        <div class='pull-right'><?php echo $treeManageLink;?></div>
+        <?php endif;?>
+      </div>
+      <div class='panel-body'><?php echo $treeModuleMenu;?></div>
     </div>
     <?php endif;?>
   </div>
