d6b1f0b1b6af73c9818f5f8a83a21fb5f84e6fa6
guanxiying <guanxiying@xirangit.com>
2014-08-19 03:53:42 +0000

    * fix commom/ bug.

--- www/template/default/view/links/index.html.php
+++ www/template/default/view/links/index.html.php
@@ -3,7 +3,7 @@ include TPL_ROOT . 'common/header.html.php';
 ?>
 <?php $common->printPositionBar($this->app->getModuleName());?>
 <div class='panel'>
-  <div class='panel-heading'><strong><i class='icon-link'></i> <?php echo $lang->links->common/?></strong></div>
+  <div class='panel-heading'><strong><i class='icon-link'></i> <?php echo $lang->links->common;?></strong></div>
   <div class='panel-body'><?php echo $links->all;?></div>
 </div>
 <?php include TPL_ROOT . 'common/footer.html.php'; ?>
