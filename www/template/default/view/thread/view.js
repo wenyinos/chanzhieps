d3ec040fe191c1fd42ca9cd4ffc4bd524a6fb3d0
daitingting <daitingting@xirangit.com>
2014-09-15 11:30:41 +0800

    * Adjust stick function.

--- www/template/default/view/thread/view.js
+++ www/template/default/view/thread/view.js
@@ -20,7 +20,7 @@ $(document).ready(function()
             }
         }
     });
-    $.setAjaxJSONER('.jsoner', function(response){ bootbox.alert(response.message, function(){location.href = response.source; return false;});});
+    $.setAjaxJSONER('.jsoner', function(response){ bootbox.alert(response.message, function(){location.href = response.locate; return true;});});
     $.setAjaxJSONER('.switcher', function(response){ bootbox.alert(response.message, function(){location.href = response.locate; return false;});});
     $('.nav-system-forum').addClass('active');
 
