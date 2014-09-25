a7feea0d763ccbcbffc0d3301a5404c4b9593e3e
daitingting <daitingting@xirangit.com>
2014-09-25 13:35:07 +0800

    * Add readonly for block which has children.

--- system/module/block/model.php
+++ system/module/block/model.php
@@ -258,7 +258,8 @@ class blockModel extends model
         $grid    = isset($block->grid) ? $block->grid : '';
 
         $entry = "<div class='block-item row' data-block='{$key}' data-id='{$blockID}'>";
-        $entry .= "<div class='col col-type'>" . html::select("blocks[{$key}]", $blockOptions, $blockID, "class='form-control block' id='block_{$key}'") . "</div>";
+        $readonly = !empty($block->children) ? "readonly='readonly'" : '';
+        $entry .= "<div class='col col-type'>" . html::select("blocks[{$key}]", $blockOptions, $blockID, "class='form-control block' id='block_{$key}' $readonly") . "</div>";
         $entry .= "<div class='col col-grid'><div class='input-group'><span class='input-group-addon'>{$this->lang->block->grid}</span>" . html::select("grid[{$key}]", $this->lang->block->gridOptions, $grid, "class='form-control'") . '</div></div>';
 
         $titlelessChecked = isset($block->titleless) && $block->titleless ? 'checked' : '';
