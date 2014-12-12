f8ad11a4374cfa59b20f4d05175192c1dcf925f0
guanxiying <guanxiying@xirangit.com>
2014-12-12 01:24:52 +0000

    * Add ranzhi form.

--- system/module/site/view/setoauth.html.php
+++ system/module/site/view/setoauth.html.php
@@ -14,50 +14,87 @@
 <div class='row'>
 <?php foreach($lang->user->oauth->providers as $providerCode => $providerName):?>
 <?php isset($this->config->oauth->$providerCode) ? $oauth = json_decode($this->config->oauth->$providerCode) : $oauth = '';?>
-<div class='col-sm-6'>
-  <div class='panel'>
-    <div class='panel-heading'>
-      <strong><i class="icon-<?php echo $providerCode; ?>"></i> <?php echo $providerName;?></strong>
-      <div class='panel-actions'>
-        <?php echo html::a('http://api.chanzhi.org/goto.php?item=help_' . $providerCode . 'oauth', '<i class="icon-question-sign"></i> ' . $lang->site->oauthHelp, "target='_blank' class='btn btn-link'");?>
+  <div class='col-sm-6 panel-box'>
+    <div class='panel'>
+      <div class='panel-heading'>
+        <strong><i class="icon-<?php echo $providerCode; ?>"></i> <?php echo $providerName;?></strong>
+        <div class='panel-actions'>
+          <?php echo html::a('http://api.chanzhi.org/goto.php?item=help_' . $providerCode . 'oauth', '<i class="icon-question-sign"></i> ' . $lang->site->oauthHelp, "target='_blank' class='btn btn-link'");?>
+        </div>
+      </div>
+      <div class='panel-body'>
+        <form method='post' id='<?php echo $providerCode;?>AjaxForm' class='form-horizontal'>
+          <table class="table table-form">
+            <tr>
+              <th class='w-100px'><?php echo $lang->user->oauth->verification;?></th>
+              <td class='w-p60'>
+                <?php echo html::input('verification', isset($oauth->verification) ? $oauth->verification : '', "class='form-control'");?>
+              </td><td></td>
+            </tr>
+            <tr>
+              <th><?php echo $lang->user->oauth->{$providerCode}->clientID;?></th>
+              <td>
+                <?php echo html::input('clientID', isset($oauth->clientID) ? $oauth->clientID : '', "class='form-control'");?>
+              </td>
+            </tr>
+            <tr>
+              <th><?php echo $lang->user->oauth->{$providerCode}->clientSecret;?></th>
+              <td>
+                <?php echo html::input('clientSecret', isset($oauth->clientSecret) ? $oauth->clientSecret : '', "class='form-control'");?>
+              </td>
+            </tr>
+            <?php if($providerCode == 'sina'):?>
+            <tr>
+              <th><?php echo $lang->user->oauth->widget;?></th>
+              <td><?php echo html::input('widget', isset($oauth->widget) ? $oauth->widget : '', "class='form-control'");?></td>
+            </tr>
+            <?php endif;?>
+            <tr>
+             <th></th> <td><?php echo html::submitButton() . html::hidden('provider', $providerCode);?></td>
+            </tr>
+          </table>
+        </form>
       </div>
     </div>
-    <div class='panel-body'>
-      <form method='post' id='<?php echo $providerCode;?>AjaxForm' class='form-horizontal'>
-        <table class="table table-form">
-          <tr>
-            <th class='w-100px'><?php echo $lang->user->oauth->verification;?></th>
-            <td class='w-p60'>
-              <?php echo html::input('verification', isset($oauth->verification) ? $oauth->verification : '', "class='form-control'");?>
-            </td><td></td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->user->oauth->{$providerCode}->clientID;?></th>
-            <td>
-              <?php echo html::input('clientID', isset($oauth->clientID) ? $oauth->clientID : '', "class='form-control'");?>
-            </td>
-          </tr>
-          <tr>
-            <th><?php echo $lang->user->oauth->{$providerCode}->clientSecret;?></th>
-            <td>
-              <?php echo html::input('clientSecret', isset($oauth->clientSecret) ? $oauth->clientSecret : '', "class='form-control'");?>
-            </td>
-          </tr>
-          <?php if($providerCode == 'sina'):?>
-          <tr>
-            <th><?php echo $lang->user->oauth->widget;?></th>
-            <td><?php echo html::input('widget', isset($oauth->widget) ? $oauth->widget : '', "class='form-control'");?></td>
-          </tr>
-          <?php endif;?>
-          <tr>
-           <th></th> <td><?php echo html::submitButton() . html::hidden('provider', $providerCode);?></td>
-          </tr>
-        </table>
-      </form>
-    </div>
   </div>
-</div>
-<?php endforeach;?>
+  <?php endforeach;?>
+
+  <div class='col-sm-8'>
+    <div class='panel'>
+      <div class='panel-heading'>
+        <strong><?php echo $lang->ranzhi->common;?></strong>
+        <div class='panel-actions'>
+        </div>
+      </div>
+      <div class='panel-body'>
+        <form action='<?php echo helper::createLink('ranzhi', 'saveConfig');?>' method='post' id='ranzhiAjaxForm'>
+          <table class='table table-form'>
+            <tr>
+              <th class='w-100px'><?php echo $lang->ranzhi->code;?></th>
+              <td><?php echo html::input('code', $this->config->ranzhi->code, "class='form-control'");?></td> 
+            </tr>
+            <tr>
+              <th><?php echo $lang->ranzhi->key;?></th>
+              <td><?php echo html::input('key', $this->config->ranzhi->key, "class='form-control'");?></td> 
+            </tr>
+            <tr>
+              <th><?php echo $lang->ranzhi->ranzhiURL;?></th>
+              <td>
+                <div class='input-group'>
+                  <?php echo html::input('ranzhiURL', $this->config->ranzhi->ranzhiURL, "class='form-control'");?>
+                  <span class='input-group-addon'> <?php echo $lang->ranzhi->requestType;?> </span>
+                  <?php echo html::select('requestType', $lang->ranzhi->requestTypeList, $this->config->ranzhi->requestType, "class='form-control'");?>
+                </div>
+              </td> 
+            </tr>
+            <tr>
+             <th></th> <td><?php echo html::submitButton() . html::hidden('provider', $providerCode);?></td>
+            </tr>
+          </table>
+        </form>
+      </div> 
+    </div> 
+  </div>
 </div>
 
 <?php include '../../common/view/footer.admin.html.php';?>
