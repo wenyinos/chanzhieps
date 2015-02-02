2a47a98df131343ac6d014503f570f0fc1efe7d1
chujilu <chujilu@cnezsoft.com>
2015-02-02 17:20:45 +0800

    * Change license.

--- system/module/upgrade/lang/en.php
+++ system/module/upgrade/lang/en.php
@@ -2,8 +2,8 @@
 /**
  * The upgrade module English file of ZenTaoPMS.
  *
- * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
- * @license     http://api.chanzhi.org/goto.php?item=license 
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv11.html)
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
  * @package     upgrade
  * @version     $Id: en.php 5119 2013-07-12 08:06:42Z wyd621@gmail.com $
