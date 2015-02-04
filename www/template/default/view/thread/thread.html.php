b450ef213a312c25222e7569ff5297c26cb82d01
chenfei <1217402763@qq.com>
2015-02-04 09:25:56 +0800

    * finish task #1148

--- www/template/default/view/thread/thread.html.php
+++ www/template/default/view/thread/thread.html.php
@@ -23,7 +23,9 @@
       </td>
       <td id='<?php echo $thread->id;?>' class='thread-wrapper'>
         <div class='thread-content article-content'><?php echo $thread->content;?></div>
+        <?php if(!empty($thread->files)):?>
         <div class='article-files'><?php $this->thread->printFiles($thread, $this->thread->canManage($board->id, $thread->author));?></div>
+        <?php endif;?>
       </td>
     </tr>
   </table>
