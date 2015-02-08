2f3d8565eb37ef6057a585eb945ae1304b845f87
chenfei <1217402763@qq.com>
2015-02-08 16:24:03 +0800

    Modify the Copyright information.

--- system/module/package/view/obtain.html.php
+++ system/module/package/view/obtain.html.php
@@ -2,12 +2,12 @@
 /**
  * The obtain view file of package module of ChanZhiEPS.
  *
- * @copyright   Copyright 2009-2010 QingDao Nature Easy Soft Network Technology Co,LTD (www.cnezsoft.com)
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
  * @license     ZPL (http://zpl.pub/page/zplv11.html) 
  * @author      Chunsheng Wang <chunsheng@xirangit.com>
  * @package     package
  * @version     $Id$
- * @link        http:www.chanzhi.org
+ * @link        http://www.chanzhi.org
  */
 ?>
 <?php include '../../common/view/header.admin.html.php';?>
