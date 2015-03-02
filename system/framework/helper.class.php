2807839c692ca3433c2a2fb6eacd60e023801ec2
guanxiying <guanxiying@xirangit.com>
2015-03-02 03:04:45 +0000

    * Add getHomeroot function.

--- system/framework/helper.class.php
+++ system/framework/helper.class.php
@@ -593,6 +593,27 @@ function getWebRoot($full = false)
 }
 
 /**
+ * Get home root.
+ * 
+ * @param  string $langCode 
+ * @access public
+ * @return string
+ */
+function getHomeRoot($langCode = '')
+{
+    global $config;
+
+    $langCode = $langCode == '' ? $config->langCode : $langCode;
+    if($langCode == $config->langsShortcuts[$config->default->lang]) return $config->webRoot;
+    $homeRoot = $config->webRoot;
+
+    if($langCode and $config->requestType == 'PATH_INFO') $homeRoot = $config->webRoot . '/' . $langCode; 
+    if($langCode and $config->requestType == 'GET')       $homeRoot = $config->webRoot .  "?{$config->langVar}=$langCode";
+    return $homeRoot;
+
+}
+
+/**
  * Check admin entry. 
  * 
  * @access public
