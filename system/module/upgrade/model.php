3bde0e9b49358232b1b66c8e575aff1e669f2a25
guanxiying <guanxiying@xirangit.com>
2015-03-02 05:09:58 +0000

    * Fix upgrade from 2.5.3 bug.

--- system/module/upgrade/model.php
+++ system/module/upgrade/model.php
@@ -87,8 +87,11 @@ class upgradeModel extends model
             case '2_5_3':
                 $this->execSQL($this->getUpgradeFile('2.5.3'));
                 $this->fixCustomedCss();
+            case '3_0';
+            case '3_0_1';
             case '3_1':
                 $this->fixBasicSite();
+            case '3_2';
             case '3_3':
                 $this->execSQL($this->getUpgradeFile('3.3'));
 
@@ -129,6 +132,10 @@ class upgradeModel extends model
             case '2_5_beta' : $confirmContent .= file_get_contents($this->getUpgradeFile('2.5.beta'));
             case '2_5_2'    : $confirmContent .= file_get_contents($this->getUpgradeFile('2.5.2'));
             case '2_5_3'    : $confirmContent .= file_get_contents($this->getUpgradeFile('2.5.3'));
+            case '3_0';
+            case '3_0_1';
+            case '3_1';
+            case '3_2';
             case '3_3'      : $confirmContent .= file_get_contents($this->getUpgradeFile('3.3'));
         }
         return str_replace(array('xr_', 'eps_'), $this->config->db->prefix, $confirmContent);
