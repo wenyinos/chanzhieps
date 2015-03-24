01625d50005b6f628a3e0c935f7c0414742bdd47
daitingting <daitingting@xirangit.com>
2015-03-24 17:11:42 +0800

    * Set default value for shell mode.

--- system/framework/router.class.php
+++ system/framework/router.class.php
@@ -705,7 +705,7 @@ class router
      */
     public function fixDomain()
     {
-        if(RUN_MODE == 'install' or RUN_MODE == 'upgrade' or !$this->config->installed) return true;
+        if(RUN_MODE == 'install' or RUN_MODE == 'upgrade' or RUN_MODE == 'shell' or !$this->config->installed) return true;
         $result = $this->dbh->query("select value from " . TABLE_CONFIG . " where owner = 'system' and module = 'common' and section = 'site' and `key` = 'domain'")->fetch();
         $mainDomain = !empty($result->value) ? $result->value : '';
         $mainDomain = str_replace('http://', '', $mainDomain);
@@ -728,7 +728,7 @@ class router
      */
     public function setClientLang($lang = '')
     {
-        if(RUN_MODE != 'install' and RUN_MODE != 'upgrade'  and $this->config->installed)
+        if(RUN_MODE != 'install' and and RUN_MODE == 'upgrade' RUN_MODE != 'shell'  and $this->config->installed)
         {
             $result = $this->dbh->query("select value from " . TABLE_CONFIG . " where owner = 'system' and module = 'common' and section = 'site' and `key` = 'defaultLang'")->fetch();
             $defaultLang = !empty($result->value) ? $result->value : $this->config->default->lang;
