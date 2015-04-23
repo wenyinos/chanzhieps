73f09a359ff884721545dbd1c02cbe35329e8927
chujilu <chujilu@cnezsoft.com>
2015-04-23 17:04:13 +0800

    * Fix a bug #168@xirang.5upm.

--- system/module/book/view/catalog.html.php
+++ system/module/book/view/catalog.html.php
@@ -22,7 +22,7 @@
         <th class='w-p10'><?php echo $lang->book->type;?></th>
         <th class='w-p10'><?php echo $lang->book->author;?></th>
         <th><?php echo $lang->book->title;?></th>
-        <th class='w-300px'><?php echo $lang->book->alias;?></th>
+        <th class='w-p20'><?php echo $lang->book->alias;?></th>
         <th class='w-p10'><?php echo $lang->book->keywords;?></th>
         <th class='w-180px'><?php echo $lang->book->addedDate;?></th>
         <th class='w-80px'><?php echo $lang->actions; ?></th>
@@ -51,7 +51,7 @@
         <td><?php echo html::select("type[]", $lang->book->typeList, '', "class='form-control'");?></td>
         <td><?php echo html::input("author[]", $app->user->realname, "class='form-control'");?></td>
         <td><?php echo html::input("title[]", '', "class='form-control'");?></td>
-        <td><?php echo html::input("alias[]", '', "class='form-control' placeholder='{$lang->alias}'");?></td>
+        <td><?php echo html::input("alias[]", '', "class='form-control' placeholder='{$lang->alias}' title='{$lang->alias}'");?></td>
         <td><?php echo html::input("keywords[]", '', "class='form-control'");?></td>
         <td><?php echo html::input("addedDate[]", helper::now(), "class='form-control date'");?></td>
         <td>
