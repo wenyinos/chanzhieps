cfad86128faad269483be583a7131054cfdb53e4
Catouse <catouse@gmail.com>
2015-05-05 14:57:26 +0800

    * finish task #1225.

--- system/module/error/lang/en.php
+++ system/module/error/lang/en.php
@@ -4,3 +4,4 @@ $lang->error->pageNotFound    = 'Page not found';
 $lang->error->articleCategory = 'Browse articles';
 $lang->error->productCategory = 'Browse products';
 $lang->error->blogCategory    = 'Browse blog';
+$lang->error->searchTip       = 'Welcome to continue browsing other content:';
--- www/template/default/view/error/index.html.php
+++ www/template/default/view/error/index.html.php
@@ -11,9 +11,25 @@
  */
 ?>
 <?php include TPL_ROOT . 'common/header.html.php';?>
-<div class='alert alert-danger'>
-  <h3><?php echo $lang->error->pageNotFound;?></h3>
-  <p><?php echo $this->config->company->desc;?></p>
+<style>
+#panel-404 {padding: 40px 80px 60px;}
+#panel-404 h1 {margin-bottom: 40px;}
+#panel-404 form {max-width: 300px}
+.screen-phone #panel-404 {padding: 20px 20px 30px;}
+</style>
+<div class='panel' id='panel-404'>
+  <h1>404 <small> - <?php echo $lang->error->pageNotFound;?></small></h1>
+  <p><small><?php echo $lang->error->searchTip;?></small></p>
+  <form action='<?php echo helper::createLink('search')?>' method='get' role='search'>
+    <div class='input-group'>
+      <?php $keywords = ($this->app->getModuleName() == 'search') ? $this->session->serachIngWord : '';?>
+      <?php echo html::input('words', $keywords, "class='form-control' placeholder=''");?>
+      <?php if($this->config->requestType == 'GET') echo html::hidden($this->config->moduleVar, 'search') . html::hidden($this->config->methodVar, 'index');?>
+      <div class='input-group-btn'>
+        <button class='btn btn-default' type='submit'><i class='icon icon-search'></i></button>
+      </div>
+    </div>
+  </form>
 </div>
 <?php $this->fetch('sitemap', 'index', 'onlyBody=yes')?>
 <?php include TPL_ROOT . 'common/footer.html.php';?>
