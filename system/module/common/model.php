644c0edf7e8b0040be98b8bd383f6d29aad526a8
guanxiying <guanxiying@xirangit.com>
2015-05-06 14:14:38 +0800

    * Add print search.

--- system/module/common/model.php
+++ system/module/common/model.php
@@ -740,6 +740,20 @@ class commonModel extends model
     }
 
     /**
+     * Print the position bar of Search. 
+     * 
+     * @param  int    $module 
+     * @param  int    $object 
+     * @param  int    $keywords 
+     * @access public
+     * @return void
+     */
+    public function printSearch($module, $object, $keywords)
+    {
+        echo "<li> {$this->lang->search->common} </li>" . "<li>{$keywords}</li>";
+    }
+
+    /**
      * Create front link for admin MODEL.
      *
      * @param string       $module
