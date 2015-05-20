c30d9f2e080cb3d91f557c743ed9bd5d1ab5a548
Catouse <catouse@gmail.com>
2015-05-20 13:31:12 +0800

    * change common/datepicker.

--- system/module/common/view/datepicker.html.php
+++ system/module/common/view/datepicker.html.php
@@ -8,7 +8,50 @@ if($clientLang != 'en') js::import($jsRoot . 'datetimepicker/js/locales/' . $cli
 <script language='javascript'>
 $(function()
 {
-    startDate = new Date(2000, 1, 1);
+    $.fn.fixedDate = function()
+    {
+        return $(this).each(function()
+        {
+            var $this = $(this);
+            if($this.offset().top + 200 > $(document.body).height())
+            {
+                $this.attr('data-picker-position', 'top-right');
+            }
+
+            if($this.val() == '0000-00-00')
+            {
+                $this.focus(function(){if($this.val() == '0000-00-00') $this.val('').datetimepicker('update');}).blur(function(){if($this.val() == '') $this.val('0000-00-00')});
+            }
+        });
+    };
+    
+    var startDate = new Date(2000, 1, 1);
+    var options = 
+    {
+        language: '<?php echo $clientLang; ?>',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1,
+        format: 'yyyy-mm-dd hh:ii',
+        startDate: startDate
+    };
+
+    var dateOptions = $.extend(options, {minView: 2, format: 'yyyy-mm-dd'});
+    var timeOptions = $.extend(options, {startView: 1, minView: 0, maxView: 1, format: 'hh:ii'});
+
+    $('.form-datetime').fixedDate().datetimepicker(options);
+    $('.form-date').fixedDate().datetimepicker($.extend(options, {minView: 2, format: 'yyyy-mm-dd'}));
+    $('.form-time').fixedDate().datetimepicker($.extend(options, {startView: 1, minView: 0, maxView: 1, format: 'hh:ii'}));
+
+    $('.datepicker-wrapper').click(function()
+    {
+        $(this).find('.form-date, .form-datetime, .form-time').datetimepicker('show').focus();
+    });
+    
     $(".date").datetimepicker
     ({
         format: 'yyyy-mm-dd hh:ii',
