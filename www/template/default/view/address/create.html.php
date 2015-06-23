c2cc90e98367f362c460dc2a0e16de804db6bc13
daitingting <daitingting@xirangit.com>
2015-06-23 14:36:44 +0800

    * Adjust license and copyright.

--- www/template/default/view/address/create.html.php
+++ www/template/default/view/address/create.html.php
@@ -2,8 +2,8 @@
 /**
  * The create view of address module of chanzhiEPS.
  *
- * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
- * @license     商业软件，非开源软件
+ * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     ZPL (http://zpl.pub/page/zplv11.html)
  * @author      Xiying Guan <guanxiying@xirangit.com>
  * @package     address 
  * @version     $Id$
