5affaf8f2906169a4cd2de45fa6eeb6ae25a5381
chujilu <chujilu@cnezsoft.com>
2015-07-02 13:11:45 +0800

    * change ok.txt to random file.

--- system/framework/helper.class.php
+++ system/framework/helper.class.php
@@ -638,6 +638,33 @@ class helper
         }
         return false;
     }
+
+    /**
+     * Create random string. 
+     * 
+     * @param  int    $length 
+     * @param  string $skip A-Z|a-z|0-9
+     * @static
+     * @access public
+     * @return void
+     */
+    public static function createRandomStr($length, $skip = '')
+    {
+        $str  = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $skip = str_replace('A-Z', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', $skip);
+        $skip = str_replace('a-z', 'abcdefghijklmnopqrstuvwxyz', $skip);
+        $skip = str_replace('0-9', '0123456789', $skip);
+        for($i = 0; $i < strlen($skip); $i++)
+        {
+            $str = str_replace($skip[$i], '', $str);
+        }
+
+        $strlen = strlen($str);
+        while($length > strlen($str)) $str .= $str;
+
+        $str = str_shuffle($str); 
+        return substr($str,0,$length); 
+    }
 }
 
 /**
