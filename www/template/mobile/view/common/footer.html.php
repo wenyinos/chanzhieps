8c1b0283277e9c4bbaea88b4e7708fe6305dfbaf
Catouse <catouse@gmail.com>
2015-07-29 11:13:07 +0800

    + add company views for mobile template.

--- www/template/mobile/view/common/footer.html.php
+++ www/template/mobile/view/common/footer.html.php
@@ -1,16 +1,9 @@
 <footer  class="appbar fix-bottom">
   <ul class="nav">
-    <li><?php echo html::a('#contactUsDialog', "<i class='icon icon-comments-alt'></i> {$lang->company->contactUs}", "class='text-primary' data-toggle='modal'");?></li>
-    <li><?php echo html::a($this->createLink('company', 'index'), "<i class='icon icon-group'></i> {$lang->aboutUs}", "class='text-important'");?></li>
+    <li><?php echo html::a($this->createLink('company', 'contact'), "<i class='icon icon-comments-alt'></i> {$lang->company->contactUs}", "class='text-primary' data-toggle='modal'");?></li>
+    <li><?php echo html::a($this->createLink('company', 'index'), "<i class='icon icon-group'></i> {$lang->aboutUs}", "class='text-important' data-toggle='modal'");?></li>
   </ul>
 </footer>
-<div class='modal fade' id='contactUsDialog'>
-  <div class='modal-dialog'>
-    <div class='modal-content'>
-      <div class='modal-header'><button type='button' class='close' data-dismiss='modal'><span aria-hidden="true">×</span></button><strong><?php echo $lang->company->contactUs;?></strong></div>
-    </div>
-  </div>
-</div>
 <?php
 if(isset($pageJS)) js::execute($pageJS);
 ?>
---
+++ www/template/mobile/view/company/index.html.php
@@ -0,0 +1,14 @@
+<div class='modal-dialog'>
+  <div class='modal-content'>
+    <div class='modal-header'>
+      <button type='button' class='close' data-dismiss='modal'><span aria-hidden='true'>×</span></button>
+      <h5 class='modal-title'><?php echo $lang->aboutUs?></h5>
+    </div>
+    <div class='modal-body'>
+      <div class='article-content'>
+        <?php echo $company->content;?>
+      </div>
+    </div>
+  </div>
+</div>
+
