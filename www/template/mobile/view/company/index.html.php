f65bb92895df9b5e5c963756d18614795a8f24da
Catouse <catouse@gmail.com>
2015-08-03 11:22:48 +0800

    * fix email error.

--- www/template/mobile/view/company/index.html.php
+++ www/template/mobile/view/company/index.html.php
@@ -5,7 +5,7 @@
  *
  * @copyright   Copyright 2009-2015 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
  * @license     ZPLV1 (http://www.chanzhi.org/license/)
- * @author      Hao Sun <daitingting@xirangit.com>
+ * @author      Hao Sun <sunhao@cnezsoft.com>
  * @package     product
  * @version     $Id$
  * @link        http://www.chanzhi.org
