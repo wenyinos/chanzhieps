ad11fae40d7735584a55d65943bb32e0e5755735
guanxiying <daitingting@xirangit.com>
2015-08-05 15:26:03 +0800

    * Add setdevice function.

--- system/module/ui/control.php
+++ system/module/ui/control.php
@@ -27,15 +27,20 @@ class ui extends control
         if($template and isset($templates[$template]))
         {  
             $setting = array();
-            $setting['name']   = $template;
-            $setting['theme']  = $theme;
-            $setting['parser'] = isset($templates[$template]['parser']) ? $templates[$template]['parser'] : 'default';
-            $setting['customTheme'] =  $custom ? $theme : '';
+            $setting['name']        = $template;
+            $setting['theme']       = $theme;
+            $setting['parser']      = isset($templates[$template]['parser']) ? $templates[$template]['parser'] : 'default';
+
+            $device = helper::getDevice();
+            $config = new stdclass();
+            $config->{$device} = $setting;
 
             $cssFile = sprintf($this->config->site->ui->customCssFile, $template, $theme);
             if(!file_exists($cssFile)) $this->ui->createCustomerCss($template, $theme);
 
-            $result = $this->loadModel('setting')->setItems('system.common.template', $setting);
+            $result = $this->loadModel('setting')->setItems('system.common.template', $config);
+            a($result);
+            a($this->config->template);exit;
             if($result) $this->send(array('result' => 'success', 'message' => $this->lang->setSuccess));
             $this->send(array('result' => 'fail', 'message' => $this->lang->fail));
         }
@@ -409,7 +414,6 @@ class ui extends control
         $setting['name']   = $packageInfo->template;
         $setting['theme']  = $packageInfo->code;
         $setting['parser'] = isset($packageInfo->parser) ? $packageInfo->parser : 'default';
-        $setting['customTheme'] =  '';
 
         $result = $this->loadModel('setting')->setItems('system.common.template', $setting);
 
@@ -430,5 +434,18 @@ class ui extends control
         $result = $this->ui->deleteTheme($template, $theme);
         if($result) $this->send(array('result' => 'success', 'message' => $this->lang->ui->deleteThemeSuccess, "locate" => inlink('setTemplate')));
         $this->send(array('result' => 'fail', 'message' => $this->lang->ui->deleteThemeFail));
+    }
+
+    /**
+     * Set device admin.
+     * 
+     * @param  string    $device 
+     * @access public
+     * @return void
+     */
+    public function setDevice($device)
+    {
+        $this->session->set('device', $device);   
+        $this->send(array('result' => 'success', 'message' => $this->lang->setSuccess));
     }
 }
