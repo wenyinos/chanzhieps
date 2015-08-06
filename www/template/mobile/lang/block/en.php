6b9f50b8c73cb12705f40eb3bf3422ab7c6407c5
Catouse <catouse@gmail.com>
2015-08-06 18:49:21 +0800

    * change block language for mobile template.

--- www/template/mobile/lang/block/en.php
+++ www/template/mobile/lang/block/en.php
@@ -9,134 +9,134 @@
  * @version     $Id$
  * @link        http://www.chanzhi.org
  */
-$lang->block->default = new stdclass();
-$lang->block->default->typeList['html']     = 'Html block';
-$lang->block->default->typeList['htmlcode'] = 'Html codes';
-$lang->block->default->typeList['phpcode']  = 'php codes';
+$lang->block->mobile = new stdclass();
+$lang->block->mobile->typeList['html']     = 'Html block';
+$lang->block->mobile->typeList['htmlcode'] = 'Html codes';
+$lang->block->mobile->typeList['phpcode']  = 'php codes';
 
-$lang->block->default->typeList['latestArticle'] = 'Latest Articles';
-$lang->block->default->typeList['hotArticle']    = 'Hot Articles';
+$lang->block->mobile->typeList['latestArticle'] = 'Latest Articles';
+$lang->block->mobile->typeList['hotArticle']    = 'Hot Articles';
 
-$lang->block->default->typeList['latestBlog']      = 'Latest Blogs';
-$lang->block->default->typeList['latestThread']    = 'Latest Threads';
+$lang->block->mobile->typeList['latestBlog']      = 'Latest Blogs';
+$lang->block->mobile->typeList['latestThread']    = 'Latest Threads';
 
-$lang->block->default->typeList['latestProduct']   = 'Latest Products';
-$lang->block->default->typeList['featuredProduct'] = 'Featured Products';
-$lang->block->default->typeList['hotProduct']      = 'Hot Products';
+$lang->block->mobile->typeList['latestProduct']   = 'Latest Products';
+$lang->block->mobile->typeList['featuredProduct'] = 'Featured Products';
+$lang->block->mobile->typeList['hotProduct']      = 'Hot Products';
 
-$lang->block->default->typeList['pageList']        = 'Page List';
+$lang->block->mobile->typeList['pageList']        = 'Page List';
 
-$lang->block->default->typeList['articleTree'] = 'Article Categories';
-$lang->block->default->typeList['productTree'] = 'Product Categories';
-$lang->block->default->typeList['blogTree']    = 'Blog Categories';
+$lang->block->mobile->typeList['articleTree'] = 'Article Categories';
+$lang->block->mobile->typeList['productTree'] = 'Product Categories';
+$lang->block->mobile->typeList['blogTree']    = 'Blog Categories';
 
-$lang->block->default->typeList['contact']  = 'Contact Us';
-$lang->block->default->typeList['followUs'] = 'Follow Us';
-$lang->block->default->typeList['about']    = 'About Us';
-$lang->block->default->typeList['links']    = 'Links';
-$lang->block->default->typeList['slide']    = 'Slide';
+$lang->block->mobile->typeList['contact']  = 'Contact Us';
+$lang->block->mobile->typeList['followUs']  = 'Follow Us';
+$lang->block->mobile->typeList['about']    = 'About Us';
+$lang->block->mobile->typeList['links']    = 'Links';
+$lang->block->mobile->typeList['slide']    = 'Slide';
 
-$lang->block->default->typeGroups = array();
-$lang->block->default->typeGroups['html']     = 'input';
-$lang->block->default->typeGroups['htmlcode'] = 'input';
-$lang->block->default->typeGroups['phpcode']  = 'input';
+$lang->block->mobile->typeGroups = array();
+$lang->block->mobile->typeGroups['html']     = 'input';
+$lang->block->mobile->typeGroups['htmlcode'] = 'input';
+$lang->block->mobile->typeGroups['phpcode']  = 'input';
 
-$lang->block->default->typeGroups['latestArticle'] = 'article';
-$lang->block->default->typeGroups['hotArticle']    = 'article';
+$lang->block->mobile->typeGroups['latestArticle'] = 'article';
+$lang->block->mobile->typeGroups['hotArticle']    = 'article';
 
-$lang->block->default->typeGroups['latestBlog']    = 'blog';
-$lang->block->default->typeGroups['latestThread']  = 'thread';
+$lang->block->mobile->typeGroups['latestBlog']    = 'blog';
+$lang->block->mobile->typeGroups['latestThread']  = 'thread';
 
-$lang->block->default->typeGroups['latestProduct']   = 'product';
-$lang->block->default->typeGroups['featuredProduct'] = 'product';
-$lang->block->default->typeGroups['hotProduct']      = 'product';
+$lang->block->mobile->typeGroups['latestProduct']   = 'product';
+$lang->block->mobile->typeGroups['featuredProduct'] = 'product';
+$lang->block->mobile->typeGroups['hotProduct']      = 'product';
 
-$lang->block->default->typeGroups['pageList']        = 'page';
+$lang->block->mobile->typeGroups['pageList']        = 'page';
 
-$lang->block->default->typeGroups['articleTree'] = 'category';
-$lang->block->default->typeGroups['productTree'] = 'category';
-$lang->block->default->typeGroups['blogTree']    = 'category';
+$lang->block->mobile->typeGroups['articleTree'] = 'category';
+$lang->block->mobile->typeGroups['productTree'] = 'category';
+$lang->block->mobile->typeGroups['blogTree']    = 'category';
 
-$lang->block->default->typeGroups['contact']  = 'system';
-$lang->block->default->typeGroups['followUs'] = 'system';
-$lang->block->default->typeGroups['about']    = 'system';
-$lang->block->default->typeGroups['links']    = 'system';
-$lang->block->default->typeGroups['slide']    = 'system';
+$lang->block->mobile->typeGroups['contact']  = 'system';
+$lang->block->mobile->typeGroups['followUs'] = 'system';
+$lang->block->mobile->typeGroups['about']    = 'system';
+$lang->block->mobile->typeGroups['links']    = 'system';
+$lang->block->mobile->typeGroups['slide']    = 'system';
 
-$lang->block->default->pages['all']            = 'All';
-$lang->block->default->pages['index_index']    = 'Home';
-$lang->block->default->pages['company_index']  = 'About Us';
+$lang->block->mobile->pages['all']            = 'All';
+$lang->block->mobile->pages['index_index']    = 'Home';
+$lang->block->mobile->pages['company_index']  = 'About Us';
 
-$lang->block->default->pages['article_browse'] = 'Browse article page';
-$lang->block->default->pages['article_view']   = 'View article page';
+$lang->block->mobile->pages['article_browse'] = 'Browse article page';
+$lang->block->mobile->pages['article_view']   = 'View article page';
 
-$lang->block->default->pages['product_browse'] = 'Browse product page';
-$lang->block->default->pages['product_view']   = 'View product page';
+$lang->block->mobile->pages['product_browse'] = 'Browse product page';
+$lang->block->mobile->pages['product_view']   = 'View product page';
 
-$lang->block->default->pages['blog_index']     = 'Browse blog page';
-$lang->block->default->pages['blog_view']      = 'View blog page';
+$lang->block->mobile->pages['blog_index']     = 'Browse blog page';
+$lang->block->mobile->pages['blog_view']      = 'View blog page';
 
-$lang->block->default->pages['forum_index']    = 'Forum home';
-$lang->block->default->pages['forum_board']    = 'Forum board';
-$lang->block->default->pages['thread_view']    = 'View thread';
-$lang->block->default->pages['search_list']    = 'Search';
+$lang->block->mobile->pages['forum_index']    = 'Forum home';
+$lang->block->mobile->pages['forum_board']    = 'Forum board';
+$lang->block->mobile->pages['thread_view']    = 'View thread';
+$lang->block->mobile->pages['search_list']    = 'Search';
 
-$lang->block->default->pages['book_index']     = 'Book';
-$lang->block->default->pages['book_browse']    = 'Book catalogue';
-$lang->block->default->pages['book_read']      = 'Book content';
+$lang->block->mobile->pages['book_index']     = 'Book';
+$lang->block->mobile->pages['book_browse']    = 'Book catalogue';
+$lang->block->mobile->pages['book_read']      = 'Book content';
 
-$lang->block->default->pages['message_index']  = 'Inquire';
+$lang->block->mobile->pages['message_index']  = 'Inquire';
 
-$lang->block->default->pages['page_view']      = 'Page';
+$lang->block->mobile->pages['page_view']      = 'Page';
 
-/* page layou>default-> list. */
-$lang->block->default->regions = new stdclass();
-$lang->block->default->regions->all['header'] = 'Header(invisible)';
-$lang->block->default->regions->all['top']    = 'Top';
-$lang->block->default->regions->all['banner'] = 'Banner';
-$lang->block->default->regions->all['bottom'] = 'Bottom';
-$lang->block->default->regions->all['footer'] = 'Footer(invisible)';
+/* page layou>mobile-> list. */
+$lang->block->mobile->regions = new stdclass();
+$lang->block->mobile->regions->all['header'] = 'Header(invisible)';
+$lang->block->mobile->regions->all['top']    = 'Top';
+$lang->block->mobile->regions->all['banner'] = 'Banner';
+$lang->block->mobile->regions->all['bottom'] = 'Bottom';
+$lang->block->mobile->regions->all['footer'] = 'Footer(invisible)';
 
-$lang->block->default->regions->index_index['top']     = 'Top';
-$lang->block->default->regions->index_index['middle']  = 'Middle';
-$lang->block->default->regions->index_index['bottom']  = 'Bottom';
+$lang->block->mobile->regions->index_index['top']     = 'Top';
+$lang->block->mobile->regions->index_index['middle']  = 'Middle';
+$lang->block->mobile->regions->index_index['bottom']  = 'Bottom';
 
-$lang->block->default->regions->company_index['top']          = 'Top';
-$lang->block->default->regions->company_index['bottom']       = 'Bottom';
+$lang->block->mobile->regions->company_index['top']          = 'Top';
+$lang->block->mobile->regions->company_index['bottom']       = 'Bottom';
 
-$lang->block->default->regions->article_browse['top']          = 'Top';
-$lang->block->default->regions->article_browse['bottom']       = 'Bottom';
+$lang->block->mobile->regions->article_browse['top']          = 'Top';
+$lang->block->mobile->regions->article_browse['bottom']       = 'Bottom';
 
-$lang->block->default->regions->article_view['top']          = 'Top';
-$lang->block->default->regions->article_view['bottom']       = 'Bottom';
+$lang->block->mobile->regions->article_view['top']          = 'Top';
+$lang->block->mobile->regions->article_view['bottom']       = 'Bottom';
 
-$lang->block->default->regions->product_browse['top']          = 'Top';
-$lang->block->default->regions->product_browse['bottom']       = 'Bottom';
+$lang->block->mobile->regions->product_browse['top']          = 'Top';
+$lang->block->mobile->regions->product_browse['bottom']       = 'Bottom';
 
-$lang->block->default->regions->product_view['top']          = 'Top';
-$lang->block->default->regions->product_view['bottom']       = 'Bottom';
+$lang->block->mobile->regions->product_view['top']          = 'Top';
+$lang->block->mobile->regions->product_view['bottom']       = 'Bottom';
 
-$lang->block->default->regions->blog_index['top']          = 'Top';
-$lang->block->default->regions->blog_index['bottom']       = 'Bottom';
+$lang->block->mobile->regions->blog_index['top']          = 'Top';
+$lang->block->mobile->regions->blog_index['bottom']       = 'Bottom';
 
-$lang->block->default->regions->blog_view['top']          = 'Top';
-$lang->block->default->regions->blog_view['bottom']       = 'Bottom';
+$lang->block->mobile->regions->blog_view['top']          = 'Top';
+$lang->block->mobile->regions->blog_view['bottom']       = 'Bottom';
 
-$lang->block->default->regions->forum_index['top']     = 'Top';
-$lang->block->default->regions->forum_index['bottom']  = 'Bottom';
-$lang->block->default->regions->forum_board['top']     = 'Top';
-$lang->block->default->regions->forum_board['bottom']  = 'Bottom';
-$lang->block->default->regions->thread_view['top']     = 'Top';
-$lang->block->default->regions->thread_view['bottom']  = 'Bottom';
+$lang->block->mobile->regions->forum_index['top']     = 'Top';
+$lang->block->mobile->regions->forum_index['bottom']  = 'Bottom';
+$lang->block->mobile->regions->forum_board['top']     = 'Top';
+$lang->block->mobile->regions->forum_board['bottom']  = 'Bottom';
+$lang->block->mobile->regions->thread_view['top']     = 'Top';
+$lang->block->mobile->regions->thread_view['bottom']  = 'Bottom';
 
-$lang->block->default->regions->book_browse['top']          = 'Top';
-$lang->block->default->regions->book_browse['bottom']       = 'Bottom';
+$lang->block->mobile->regions->book_browse['top']          = 'Top';
+$lang->block->mobile->regions->book_browse['bottom']       = 'Bottom';
 
-$lang->block->default->regions->book_read['top']       = 'Top';
-$lang->block->default->regions->book_read['bottom']    = 'Bottom';
+$lang->block->mobile->regions->book_read['top']       = 'Top';
+$lang->block->mobile->regions->book_read['bottom']    = 'Bottom';
 
-$lang->block->default->regions->message_index['top']          = 'Top';
-$lang->block->default->regions->message_index['bottom']       = 'Bottom';
+$lang->block->mobile->regions->message_index['top']          = 'Top';
+$lang->block->mobile->regions->message_index['bottom']       = 'Bottom';
 
-$lang->block->default->regions->page_view['top']          = 'Top';
-$lang->block->default->regions->page_view['bottom']       = 'Bottom';
+$lang->block->mobile->regions->page_view['top']          = 'Top';
+$lang->block->mobile->regions->page_view['bottom']       = 'Bottom';
