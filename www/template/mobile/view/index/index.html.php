a68f6365ea1a26401ff5b1da06be7e7a56e6afb4
Catouse <catouse@gmail.com>
2015-08-06 18:49:41 +0800

    * change index UI.

--- www/template/mobile/view/index/index.html.php
+++ www/template/mobile/view/index/index.html.php
@@ -12,4 +12,16 @@
 ?>
 <?php include TPL_ROOT . 'common/header.html.php';?>
 
+<div id='focus'>
+  <div id='focusTop' class='focus-region focus-top'>
+    <?php $this->block->printRegion($layouts, 'index_index', 'top', false);?>
+  </div>
+  <div id='focusMiddle' class='focus-region focus-middle'>
+    <?php $this->block->printRegion($layouts, 'index_index', 'middle', false);?>
+  </div>
+  <div id='focusBottom' class='focus-region focus-bottom'>
+    <?php $this->block->printRegion($layouts, 'index_index', 'bottom', false);?>
+  </div>
+</div>
+
 <?php include TPL_ROOT . 'common/footer.html.php';?>
