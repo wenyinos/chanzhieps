763729803c80396fb58df051f61f5ddcb2e9bd8a
Catouse <catouse@gmail.com>
2015-08-07 16:17:06 +0800

    + add search index view for mobile template.

--- www/template/mobile/view/common/header.html.php
+++ www/template/mobile/view/common/header.html.php
@@ -20,10 +20,24 @@
       ?></a>
   </div>
   <div class='appbar-actions'>
+    <?php if(commonModel::isAvailable('search')):?>
     <div class='dropdown'>
-      <?php if(commonModel::isAvailable('search')):?>
-      <button type='button' class='btn'><i class='icon-search'></i></button>
-      <?php endif; ?>
+      <button type='button' class='btn' data-toggle='dropdown' id='searchToggle'><i class='icon-search'></i></button>
+      <div class='dropdown-menu fade search-bar' id='searchbar'>
+        <form action='<?php echo helper::createLink('search')?>' method='get' role='search'>
+          <div class='input-group'>
+            <?php $keywords = ($this->app->getModuleName() == 'search') ? $this->session->serachIngWord : '';?>
+            <?php echo html::input('words', $keywords, "class='form-control' placeholder=''");?>
+            <?php if($this->config->requestType == 'GET') echo html::hidden($this->config->moduleVar, 'search') . html::hidden($this->config->methodVar, 'index');?>
+            <div class='input-group-btn'>
+              <button class='btn default' type='submit'><i class='icon icon-search'></i></button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <?php endif; ?>
+    <div class='dropdown'>
       <button type='button' class='btn' data-toggle='dropdown'><i class='icon-bars'></i></button>
       <ul class='dropdown-menu pull-right'>
         <?php echo commonModel::printTopBar(true);?>
