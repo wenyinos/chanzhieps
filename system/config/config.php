9ffb3ccb13c27dc189cb856652711b230d514fa4
daitingting <daitingting@xirangit.com>
2015-08-07 09:40:28 +0800

    * Use template of desktop if template of mobile is not setted.

--- system/config/config.php
+++ system/config/config.php
@@ -46,11 +46,8 @@ $config->site->importantValidate = 'okFile,email';
 
 $config->template = new stdclass();
 $config->template->desktop = new stdclass();
-$config->template->mobile  = new stdclass();
 $config->template->desktop->name  = 'default';   // Supported themes.
 $config->template->desktop->theme = 'default';   // Supported themes.
-$config->template->mobile->name   = 'default';    // Supported themes.
-$config->template->mobile->theme  = 'default';   // Supported themes.
 $config->template->parser         = 'default';   // Default parser.
 $config->template->customVersion  = '';
 
