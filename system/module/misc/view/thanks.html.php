64b3d7ec3424c2367bccd7509d526ca0cfcbe87c
qiaqia <liqiaqia@cnezsoft.com>
2015-08-13 15:50:18 +0800

    * Add zzsec on thanks page.

--- system/module/misc/view/thanks.html.php
+++ system/module/misc/view/thanks.html.php
@@ -1,6 +1,6 @@
 <?php include '../../common/view/header.modal.html.php';?>
 <article class='text-center'>
-  <h2 class='text-success'><?php echo $lang->misc->thanksTitle;?> </h2>
+  <h2 class='text-success'><strong><?php echo $lang->misc->thanksTitle;?></strong></h2>
   <p><?php echo $lang->misc->thanksSubtitle?></p>
 <div class='row'>
   <?php foreach($this->config->thanksList as $item => $link):?>
@@ -10,6 +10,9 @@
     </a>
   </div>
   <?php endforeach;?>
+</div>
+<div>
+  <?php printf($lang->misc->thanksFooter, '<a href="http://www.zzsec.com/" style="color: green"><strong>' . $lang->misc->thanksObjectName . '</strong></a>');?>
 </div>
   
 <?php include '../../common/view/footer.modal.html.php';?>
