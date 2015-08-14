7ff9505d1dad7a4233dc35e33d04696a215ca99b
Catouse <catouse@gmail.com>
2015-08-14 09:35:45 +0800

    + add company/contact view to default template.

---
+++ www/template/default/view/company/contact.html.php
@@ -0,0 +1,23 @@
+<?php 
+include TPL_ROOT . 'common/header.html.php';
+?>
+<?php $common->printPositionBar($this->app->getModuleName());?>
+<div class='panel'>
+  <div class='panel-heading'>
+    <strong><i class='icon icon-comments-alt'></i> <?php echo $lang->company->contact;?></strong>
+    <?php if(!empty($block->content->moreText) and !empty($block->content->moreUrl)):?>
+    <div class='pull-right'><?php echo html::a($block->content->moreUrl, $block->content->moreText);?></div>
+    <?php endif;?>
+  </div>
+  <div class='panel-body'>
+    <table class='table table-data'>
+      <?php foreach($contact as $item => $value):?>
+      <tr>
+        <th><?php echo $this->lang->company->$item . $this->lang->colon;?></th>
+        <td><?php echo $value;?></td>
+      </tr>
+      <?php endforeach;?>
+    </table>
+  </div>
+</div>
+<?php include TPL_ROOT . 'common/footer.html.php'; ?>
