c62172d35bf61236d6d93bd91bd5bd39b939ffee
daitingting <daitingting@xirangit.com>
2015-08-27 17:13:00 +0800

    * Adjust code.

--- system/lib/dao/dao.class.php
+++ system/lib/dao/dao.class.php
@@ -1291,6 +1291,7 @@ class sql
      */
     public function markLeft($count = 1)
     {
+        if($this->inCondition and !$this->conditionIsTrue) return $this;
         $this->sql .= str_repeat('(', $count);
         $this->inMark = true;
         return $this;
@@ -1305,6 +1306,7 @@ class sql
      */
     public function markRight($count = 1)
     {
+        if($this->inCondition and !$this->conditionIsTrue) return $this;
         $this->sql .= str_repeat(')', $count);
         $this->inMark = false;
         return $this;
