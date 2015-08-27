3b109ea940260fd7df907b265d4d1bddfef41d50
daitingting <daitingting@xirangit.com>
2015-08-27 09:30:51 +0800

    * Fix a bug for checkDomain.

--- system/module/common/model.php
+++ system/module/common/model.php
@@ -254,7 +254,7 @@ class commonModel extends model
         if(RUN_MODE == 'install' or RUN_MODE == 'upgrade' or RUN_MODE == 'shell' or RUN_MODE == 'admin' or !$this->config->installed) return true;
 
         $httpHost   = $this->server->http_host;
-        $currentURI = getWebRoot(true) . $this->app->getURI();
+        $currentURI = $httpHost . $this->server->request_uri;
         $scheme     = isset($this->config->site->scheme) ? $this->config->site->scheme : 'http';
         $mainDomain = isset($this->config->site->domain) ? $this->config->site->domain : '';
         $mainDomain = str_replace(array('http://', 'https://'), '', $mainDomain);
