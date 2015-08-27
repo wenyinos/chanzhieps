21d7da1000d6e368bb2137993bf05365096c3320
qiaqia <liqiaqia@cnezsoft.com>
2015-08-27 14:00:35 +0800

    * Modify slide.

--- system/module/slide/control.php
+++ system/module/slide/control.php
@@ -136,8 +136,11 @@ class slide extends control
     public function createGroup()
     {
         $result = $this->loadModel('tree')->createSlideGroup();
-        if($result) $this->send(array('result' => 'success', 'message' => $this->lang->createSuccess, 'locate' => inlink('admin')));
-        $this->send(array('result' => 'fail', 'message' => dao::getError()));
+        if($_POST)
+        {
+            if($result) $this->send(array('result' => 'success', 'message' => $this->lang->createSuccess, 'locate' => inlink('admin')));
+            $this->send(array('result' => 'fail', 'message' => dao::getError()));
+        }
 
         $this->view->title = $this->lang->slide->createGroup;
         $this->display();     
