2a5d0dc4b6b4918b6e1b0c6e5755d8adac26c9f7
Catouse <catouse@gmail.com>
2015-08-27 12:03:01 +0800

    * update mzui: change footer UI.

--- www/template/mobile/view/common/footer.html.php
+++ www/template/mobile/view/common/footer.html.php
@@ -1,19 +1,19 @@
 <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <div class='block-region region-all-bottom'><?php $this->loadModel('block')->printRegion($layouts, 'all', 'bottom');?></div>
-<div class='appinfo'>
-  <div class='copyright'>
+<div class='appinfo clearfix'>
+  <div class='copyright pull-left'>
     <?php
       $copyright = empty($config->site->copyright) ? '' : $config->site->copyright . '-';
-      $contact   = json_decode($config->company->contact); 
+      $contact   = json_decode($config->company->contact);
       $company   = (empty($contact->site) or $contact->site == $this->server->http_host) ? $config->company->name : html::a('http://' . $contact->site, $config->company->name, "target='_blank'");
       echo "&copy; {$copyright}" . date('Y') . ' ' . $company . '&nbsp;&nbsp;';
       ?>
   </div>
-  <div class='icpinfo'>
+  <div class='icpinfo hide'>
     <?php if(!empty($config->site->icpLink) and !empty($config->site->icpSN)) echo html::a(strpos($config->site->icpLink, 'http://') !== false ? $config->site->icpLink : 'http://' . $config->site->icpLink, $config->site->icpSN, "target='_blank'");?>
     <?php if(empty($config->site->icpLink) and !empty($config->site->icpSN))  echo $config->site->icpSN;?>
   </div>
-  <div class='powerby'>
+  <div class='powerby pull-right'>
     <?php printf($lang->poweredBy, $config->version, k(), "<object data='{$templateCommonRoot}img/chanzhi.xml' type='image/svg+xml'>{$lang->chanzhiEPSx}</object> <span class='name hide'>" . $lang->chanzhiEPSx . '</span>' . $config->version); ?>
   </div>
 </div>
