eba72cee1aa9ab4fe5592be70a99102c3bc7a27c
daitingting <daitingting@xirangit.com>
2015-08-30 17:49:13 +0800

    * Update browser and os when login. * Update browser and os when login.  * * Update browser and os when login. * Update browser and os when  * login. * Update browser and os when login. * Update browser and os  * when login. * Update browser and os when login. * Update browser and  * os when login. * Update browser and os when login.

--- system/db/upgrade4.4.sql
+++ system/db/upgrade4.4.sql
@@ -1,1 +1,5 @@
 ALTER TABLE `eps_order` CHANGE `address` `address` text NOT NULL;
+ALTER TABLE `eps_user`
+ADD `os` char(30) NOT NULL,
+ADD `browser` char(30) NOT NULL,
+ADD `browserVersion` char(30) NOT NULL;
