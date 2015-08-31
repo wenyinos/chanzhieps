957a95e90270aa641880dddb32a7237e95b81a7b
guanxiying <daitingting@xirangit.com>
2015-08-31 18:20:52 +0800

    * Fix subnav bnubg.

--- www/template/default/view/common/header.html.php
+++ www/template/default/view/common/header.html.php
@@ -34,14 +34,14 @@ if(isset($pageCSS)) css::internal($pageCSS);
       <ul class='nav navbar-nav'>
         <?php foreach($topNavs as $nav1):?>
           <?php if(empty($nav1->children)):?>
-            <li class='<?php echo $nav1->class?>'><?php echo html::a($nav1->url, $nav1->title, ($nav1->target != 'modal') ? "target='$nav1->target'" : "data-toggle='modal'");?></li>
+            <li class='<?php echo $nav1->class?>'><?php echo html::a($nav1->url, $nav1->title, "target='$nav1->target'");?></li>
             <?php else: ?>
               <li class="dropdown <?php echo $nav1->class?>">
-                <?php echo html::a($nav1->url, $nav1->title . " <b class='caret'></b>", 'class="dropdown-toggle" data-toggle="dropdown" ' . ($nav1->target != 'modal') ? "target='$nav1->target'" : "data-toggle='modal'");?>
+                <?php echo html::a($nav1->url, $nav1->title . " <b class='caret'></b>", 'class="dropdown-toggle" data-toggle="dropdown"');?>
                 <ul class='dropdown-menu' role='menu'>
                   <?php foreach($nav1->children as $nav2):?>
                     <?php if(empty($nav2->children)):?>
-                      <li class='<?php echo $nav2->class?>'><?php echo html::a($nav2->url, $nav2->title, ($nav2->target != 'modal') ? "target='$nav2->target'" : '');?></li>
+                      <li class='<?php echo $nav2->class?>'><?php echo html::a($nav2->url, $nav2->title, "target='$nav2->target'");?></li>
                     <?php else: ?>
                       <li class='dropdown-submenu <?php echo $nav2->class?>'>
                         <?php echo html::a($nav2->url, $nav2->title, ($nav2->target != 'modal') ? "target='$nav2->target'" : '');?>
