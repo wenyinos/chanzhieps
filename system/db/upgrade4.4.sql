8e08bbb2cd82e3ca2b39bca2d1e934ec4d3b1aa6
guanxiying <daitingting@xirangit.com>
2015-08-31 14:58:06 +0800

    * Merge browser and browserversion.

--- system/db/upgrade4.4.sql
+++ system/db/upgrade4.4.sql
@@ -1,5 +1,4 @@
 ALTER TABLE `eps_order` CHANGE `address` `address` text NOT NULL;
 ALTER TABLE `eps_user`
 ADD `os` char(30) NOT NULL,
-ADD `browser` char(30) NOT NULL,
-ADD `browserVersion` char(30) NOT NULL;
+ADD `browser` varchar(100) NOT NULL;
