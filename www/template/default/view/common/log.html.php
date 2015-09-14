0f0092c7d6b811b9db00f180072bcd453b7974e4
guanxiying <daitingting@xirangit.com>
2015-09-14 19:24:41 +0800

    * Add log codes.

--- www/template/default/view/common/log.html.php
+++ www/template/default/view/common/log.html.php
@@ -1,5 +1,10 @@
+<?php 
+$referer     = $this->server->http_referer;
+$refererInfo = parse_url($referer);
+if($this->server->http_host == $refererInfo['host']) $referer = '';
+?>
 <script>
-var referer         = "<?php echo urlencode($this->server->http_referer);?>";
+var referer         = "<?php echo urlencode($referer);?>";
 var browserLanguage = navigator.language || navigator.userLanguage; 
 var resolution      = screen.availWidth + ' X ' + screen.availHeight;
 $.post(createLink('log', 'record'), {referer:referer, browserLanguage:browserLanguage, resolution:resolution});
