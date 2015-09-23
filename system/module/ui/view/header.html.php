cfcc585c193aa2cdf9a193bbb06a52f05eacb10a
Catouse <catouse@gmail.com>
2015-09-23 14:59:02 +0800

    * change visual edit button.

--- system/module/ui/view/header.html.php
+++ system/module/ui/view/header.html.php
@@ -67,6 +67,7 @@
   <?php if($moduleMenu) echo $moduleMenu;?>
   <div class="pull-right">
     <ul class="nav">
+      <li><?php echo html::a(helper::createLink('visual', 'index'), '<i class="icon-magic"></i> ' . $lang->visualEdit, "target='_blank' class='navbar-link'");?></li>
       <li><?php commonModel::printLink('package', 'upload', '', '<i class="icon-download-alt"></i> ' . $lang->ui->installTemplate, "data-toggle='modal' data-width='600'")?></li>
       <li><?php commonModel::printLink('ui', 'uploadTheme', '', '<i class="icon-download-alt"></i> ' . $lang->ui->uploadTheme, "data-toggle='modal' data-width='600'")?></li>
       <li><?php commonModel::printLink('ui', 'exportTheme', '', '<i class="icon-upload-alt"></i> ' . $lang->ui->exportTheme, "data-toggle='modal' data-width='600'")?></li>
