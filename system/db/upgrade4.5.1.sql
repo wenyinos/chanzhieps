ec5dc3328d586055c965c958230e0d6570b521c1
guanxiying <daitingting@xirangit.com>
2015-10-14 15:26:59 +0800

    * Add fix domain and link fields sql.

--- system/db/upgrade4.5.1.sql
+++ system/db/upgrade4.5.1.sql
@@ -1,1 +1,2 @@
-update eps_article set sticky = '0' where sticky = '';
+UPDATE `eps_article` set sticky = '0' where sticky = '';
+UPDATE `eps_statlog` as l, `eps_statreferer` as r SET l.link = r.url where l.link = '1' and l.referer = r.id;
