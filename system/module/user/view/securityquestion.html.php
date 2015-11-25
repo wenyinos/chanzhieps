623e29b106f65eedbf1371b61da8119688724ce8
xia0ta0 <xia0ta0@outlook.com>
2015-11-25 16:25:22 +0800

    * finish task#1719.

--- system/module/user/view/securityquestion.html.php
+++ system/module/user/view/securityquestion.html.php
@@ -1,10 +1,15 @@
 <?php include '../../common/view/header.modal.html.php';?>
 <?php js::import($jsRoot . 'fingerprint/fingerprint.js');?>
-<?php if($pass):?>
 <form method='post' action='<?php echo inlink('securityquestion');?>' id='questionForm' class='form' data-checkfingerprint='1'>
   <table class='table table-form borderless'>
     <tr>
-      <th class='w-100px'><?php echo $lang->user->question;?></th>
+      <th class='w-100px'><?php echo $lang->user->password;?></th>
+      <td>
+        <?php echo html::password('oldPwd', '', "class='form-control' placeholder='{$lang->user->placeholder->password}'");?>
+      </td>
+    </tr>
+    <tr>
+      <th><?php echo $lang->user->question;?></th>
       <td><?php echo html::input('question','', "class='form-control'");?></td>
     </tr>
     <tr>
@@ -16,10 +21,4 @@
     </tr>
   </table>
 </form>
-<?php else:?>
-<?php
-$url = helper::safe64Encode($this->createLink('user', 'changepassword'));
-include '../../mail/view/captcha.html.php';
-?>
-<?php endif;?>
 <?php include '../../common/view/footer.modal.html.php';?>
