35f235126b9460453dcf2ae647721249b1ea009f
xia0ta0 <xia0ta0@outlook.com>
2015-12-10 10:38:34 +0800

    - rm maketrunk.sh.

--- maketrunk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git archive --format=zip --output=code.zip master
-rm -rf /tmp/chanzhirelease
-mkdir /tmp/chanzhirelease
-mv code.zip /tmp/chanzhirelease
-cd /tmp/chanzhirelease
-unzip code.zip
-rm /tmp/chanzhirelease/code.zip
-make
